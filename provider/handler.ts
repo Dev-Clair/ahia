deb9433191b04bf77b4705a111cea80e80bcf5a5
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-13 14:42:02 +0100

    fix: adding default port

--- provider/handler.ts
+++ provider/handler.ts
@@ -6,10 +6,8 @@ import Connection from "./connection";
 Connection(Config.MONGO_URI);
 
 if (Config.NODE_ENV === "development") {
-  app.listen(Config.PORT || 3999, () => {
-    console.log(
-      `Server process started, listening on port ${Config.PORT || 3999}`
-    );
+  app.listen(Config.PORT, () => {
+    console.log(`Server process started, listening on port ${Config.PORT}`);
   });
 } else {
   exports.provider = serverless(app);
