8f0e0c31a7556aad3df5c9552188f0bbabe456ac
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-17 00:47:49 +0100

    feat: create listing model

---
+++ listing/src/model/listingModel.ts
@@ -0,0 +1,103 @@
+import mongoose, { Schema } from "mongoose";
+import ListingInterface from "../interface/listingInterface";
+
+const ListingSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  cost: {
+    type: Number,
+    required: true,
+  },
+  purpose: {
+    type: String,
+    enum: ["Rent", "Sell"],
+    required: true,
+  },
+  listingType: {
+    type: String,
+    enum: ["Developed", "Undeveloped"],
+    required: true,
+  },
+  listingUseType: {
+    type: String,
+    enum: [
+      "single-room",
+      "mini-flat",
+      "flats",
+      "duplex",
+      "semi-detached",
+      "short-lets",
+      "office",
+      "shop",
+      "event halls",
+      "bare-land",
+    ],
+    required: true,
+  },
+  listingFeatures: [
+    {
+      type: String,
+      enum: [
+        "water",
+        "power",
+        "access roads",
+        "security",
+        "proximity to landmark and essential services",
+      ],
+    },
+  ],
+  listingLocation: {
+    address: {
+      type: String,
+      required: true,
+    },
+    zone: {
+      type: String,
+      required: true,
+    },
+    countyLGA: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+  },
+  listingUseCategory: {
+    type: String,
+    enum: ["Residential", "Commercial"],
+    required: true,
+  },
+  listingImage: {
+    type: String,
+  },
+  listingVideo: {
+    type: String,
+  },
+  listingProvider: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model<ListingInterface>("Listing", ListingSchema);
