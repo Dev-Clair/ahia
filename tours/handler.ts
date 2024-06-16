2809d46b2e610feb0ad1064c47c4e0c8f72d0802
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-16 23:51:38 +0100

    feat: starter files

---
+++ tours/handler.ts
@@ -0,0 +1,18 @@
+import serverless from "serverless-http";
+import app from "./app";
+import Config from "./config";
+import Connection from "./connection";
+
+Connection(Config.MONGO_URI);
+
+if (Config.NODE_ENV === "development") {
+  app.listen(Config.SERVER_PORT, () => {
+    console.log(
+      `Server process started, listening on port ${Config.SERVER_PORT}`
+    );
+  });
+} else {
+  const iam = serverless(app);
+
+  // export default iam;
+}
