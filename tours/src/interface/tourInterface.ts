7326e8572f646e8d5cddd02738f5356033fef29e
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-16 23:55:16 +0100

    feat: create interface

---
+++ tours/src/interface/tourInterface.ts
@@ -0,0 +1,13 @@
+import { Document } from "mongoose";
+
+export default interface TourInterface extends Document {
+  realtorId?: string;
+  customerId: string;
+  listingIds: string[];
+  zone: string;
+  countyLGA: string;
+  country: string;
+  scheduledDate: Date;
+  status: "pending" | "completed" | "cancelled";
+  createdAt: Date;
+}
