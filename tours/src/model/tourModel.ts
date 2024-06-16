ee99a193b26118297ef4b6724e940f7c53d53395
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-16 23:55:29 +0100

    feat: create model

---
+++ tours/src/model/tourModel.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import TourInterface from "../interface/tourInterface";
+
+const TourSchema: Schema<TourInterface> = new Schema({
+  realtorId: {
+    type: String,
+    required: false,
+  },
+  customerId: {
+    type: String,
+    required: true,
+  },
+  listingIds: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  zone: {
+    type: String,
+    required: true,
+  },
+  countyLGA: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  scheduledDate: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "completed", "cancelled"],
+    default: "pending",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model<TourInterface>("Tour", TourSchema);
