dedff202badb751b92098d7fdfef6fa56394168d
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-19 20:41:07 +0100

    feat: set up router and request handlers

---
+++ tours/src/controller/v1/tourController.ts
@@ -0,0 +1,179 @@
+import { NextFunction, Request, Response } from "express";
+import Tour from "../../model/tourModel";
+
+/**
+ * Retrieve collection of tours based on search parameter.
+ */
+const retrieveToursSearch = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const tours = await Tour.find({
+      //   $text: { $search: req.query.q, $caseSensitive: false },
+    });
+
+    if (!tours) {
+      return res
+        .status(404)
+        .json({ message: `No tours found for query: ${req.query.q}` });
+    }
+
+    return res.status(200).json({
+      count: tours.length,
+      tours: tours,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+/**
+ * Retrieve a tour item using its :id.
+ */
+const retrieveTourItem = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const tour = await Tour.findById({ _id: req.params.id });
+
+    if (!tour) {
+      return res
+        .status(404)
+        .json({ message: `No item found for tour id: ${req.params.id}` });
+    }
+
+    return res.status(200).json({ tour: tour });
+  } catch (err) {
+    next(err);
+  }
+};
+
+/**
+ * Replace a tour item using its :id.
+ */
+const replaceTour = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const tour = await Tour.findOneAndReplace(
+      { _id: req.params._id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    if (!tour) {
+      return res.status(404).json({
+        message: `No item found for tour id: ${req.params.id}`,
+      });
+    }
+
+    return res.status(204).json(null);
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+/**
+ * Updates a tour item using its :id.
+ */
+const updateTour = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const tour = await Tour.findOneAndUpdate(
+      { _id: req.params._id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    if (!tour) {
+      return res.status(404).json({
+        message: `No item found for tour id: ${req.params.id}`,
+      });
+    }
+
+    return res.status(204).json(null);
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+/**
+ * Complete a tour item using its :id.
+ */
+const completeTour = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const tour = await Tour.findOneAndUpdate(
+      { _id: req.params._id },
+      { status: "completed" },
+      {
+        new: true,
+      }
+    );
+
+    if (!tour) {
+      return res.status(404).json({
+        message: `No item found for tour id: ${req.params.id}`,
+      });
+    }
+
+    return res.status(204).json(null);
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+/**
+ * Cancels a tour item using its :id.
+ */
+const cancelTour = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const tour = await Tour.findOneAndUpdate(
+      { _id: req.params._id },
+      { status: "cancelled" },
+      {
+        new: true,
+      }
+    );
+
+    if (!tour) {
+      return res.status(404).json({
+        message: `No item found for tour id: ${req.params.id}`,
+      });
+    }
+
+    return res.status(204).json(null);
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+/**
+ * Handle not allowed operations
+ */
+const operationNotAllowed = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  return res.status(405).json({
+    message: "operation not allowed",
+  });
+};
+
+export default {
+  retrieveToursSearch,
+  retrieveTourItem,
+  replaceTour,
+  updateTour,
+  cancelTour,
+  operationNotAllowed,
+};
--- tours/src/router/v1/tourRouter.ts
+++ tours/src/router/v1/tourRouter.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import tourController from "../../controller/v1/tourController";
 
 const routerV1 = Router();
+
+routerV1.route("/search").get(tourController.retrieveToursSearch);
+
+routerV1
+  .route("/:id")
+  .put(tourController.replaceTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.cancelTour);
 
 export default routerV1;
