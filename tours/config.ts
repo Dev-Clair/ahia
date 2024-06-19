b756eac49ee7bcd1b916f37a61cd4987528d7226
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-19 20:41:29 +0100

    fix: modify serverport

--- tours/config.ts
+++ tours/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const Config = {
   NODE_ENV: process.env.NODE_ENV || "",
-  SERVER_PORT: process.env.SERVER_PORT || 3999,
+  SERVER_PORT: process.env.SERVER_PORT || 5999,
   MONGO_URI: process.env.MONGO_URI || "",
   AWS_REGION: process.env.AWS_REGION || "",
   AWS_ACCOUNT_ID: process.env.AWS_ACCOUNT_ID || "",
