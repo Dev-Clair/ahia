11303575012d91915996288f5224610d8fc3bea0
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-20 18:09:15 +0100

    fix: modify status code

--- tours/src/router/index.ts
+++ tours/src/router/index.ts
@@ -1,10 +1,11 @@
 import { Request, Response, Router, NextFunction } from "express";
 import routerV1 from "./v1/tourRouter";
 import routerV2 from "./v2/tourRouter";
+import HttpStatusCode from "../enum/httpStatusCode";
 import APIError from "../error/apiError";
 import NotFoundError from "../error/notfoundError";
 import BadRequestError from "../error/badrequestError";
-import globalErrorHandler from "../middleware/globalErrorHandlingMiddleware.ts";
+import GlobalErrorHandler from "../middleware/globalErrorHandlingMiddleware.ts";
 
 const router = Router();
 
@@ -14,28 +15,31 @@ router.use("/v2/tours", routerV2);
 
 router.all("*", (req: Request, res: Response, next: NextFunction) => {
   next(
-    new NotFoundError(`No resource or route defined for ${req.originalUrl}`)
+    new NotFoundError(
+      HttpStatusCode.NOT_FOUND,
+      `No resource or route defined for ${req.originalUrl}`
+    )
   );
 });
 
 router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof SyntaxError) {
-    next(new BadRequestError("Bad JSON"));
+    next(new BadRequestError(HttpStatusCode.BAD_REQUEST, "Bad JSON"));
   }
 
   next(err);
 });
 
 router.use((err: APIError, req: Request, res: Response, next: NextFunction) => {
-  if (globalErrorHandler.isTrustedError(err)) {
+  if (GlobalErrorHandler.isTrustedError(err)) {
     return res
       .status(err.httpStatusCode)
       .json({ error: err.name, message: err.message });
   }
 
-  globalErrorHandler.handleAPIError(err);
+  GlobalErrorHandler.handleAPIError(err);
 
-  res.status(500).json({
+  res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
     error: "INTERNAL SERVER ERROR",
     message:
       "Oops! Sorry an error occured on our end, we cannot process your request at this time. Please try again shortly.",
