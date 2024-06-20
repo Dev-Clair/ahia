436beb39eb7ea1ea8ed3de1b0fe434da028d6ad7
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-20 16:53:30 +0100

    fix: modify event handler

--- tours/tourCommand.ts
+++ tours/tourCommand.ts
@@ -1,18 +1,18 @@
 import Tour from "./src/model/tourModel";
 
-const tourCommand = async (command: Object) => {
+const tourCommand = (command: Object) => {
   createTour(command);
 };
 
-const createTour = async (command: Object) => {
-  try {
-    const newTour = await Tour.create(command);
-    console.log(
-      `CREATE: Success | TOUR ID: ${newTour._id} | CUSTOMER ID: ${newTour.customerId}.`
-    );
-  } catch (err: any) {
-    console.error(`CREATE: Failure | ERROR: ${err.message}`);
-  }
+const createTour = (command: Object) => {
+  Tour.create(command)
+    .then((newTour) => {
+      console.log(
+        `CREATE: Success | TOUR ID: ${newTour._id} | CUSTOMER ID: ${newTour.customerId}.`
+      );
+    })
+    .catch((err) => {
+      console.error(`CREATE: Failure | ERROR: ${err.message}`);
+    });
 };
-
 export default tourCommand;
