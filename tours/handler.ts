994e47a9598f7fb02e935071dab18a8050e5d514
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-20 16:52:59 +0100

    fix: refactor handler

--- tours/handler.ts
+++ tours/handler.ts
@@ -6,36 +6,31 @@ import tourCommand from "./tourCommand";
 
 Connection(Config.MONGO_URI);
 
-// app.listen(Config.SERVER_PORT, () => {
-//   console.log(
-//     `Server process started, listening on port ${Config.SERVER_PORT}`
-//   );
-// });
-
-const tourAPI = serverless(app);
-
-const tour = async (event: any, context: any) => {
-  if (event.source && event["detail-type"]) {
-    let payload;
-
-    payload = JSON.parse(event.detail);
-
-    const { fullDocument: paymentDetails } = payload;
-
-    const { customerId: customerId, listingsId: listingIds } = paymentDetails;
-
-    const command: {
-      customerId: string;
-      listingIds: string[];
-    } = {
-      customerId: customerId,
-      listingIds: listingIds,
-    };
-
-    tourCommand(command);
-  } else {
-    return tourAPI(event, context);
-  }
-};
-
-export default tour;
+if (Config.NODE_ENV === "development") {
+  app.listen(Config.SERVER_PORT, () => {
+    console.log(
+      `Server process started, listening on port ${Config.SERVER_PORT}`
+    );
+  });
+} else {
+  // const tourAPI = serverless(app);
+  // const tour = async (event: any, context: any) => {
+  //   if (event.source && event["detail-type"]) {
+  //     let payload;
+  //     payload = JSON.parse(event.detail);
+  //     const { fullDocument: paymentDetails } = payload;
+  //     const { customerId: customerId, listingsId: listingIds } = paymentDetails;
+  //     const command: {
+  //       customerId: string;
+  //       listingIds: string[];
+  //     } = {
+  //       customerId: customerId,
+  //       listingIds: listingIds,
+  //     };
+  //     tourCommand(command);
+  //   } else {
+  //     return tourAPI(event, context);
+  //   }
+  // };
+  // export default tour;
+}
