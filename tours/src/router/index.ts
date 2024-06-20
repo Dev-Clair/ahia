4d65f563fbd43a09056cbdb8e440136e2bd49cd6
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-20 16:35:00 +0100

    fix: properly handle error propagation

--- tours/src/router/index.ts
+++ tours/src/router/index.ts
@@ -1,10 +1,9 @@
 import { Request, Response, Router, NextFunction } from "express";
 import routerV1 from "./v1/tourRouter";
 import routerV2 from "./v2/tourRouter";
-import BaseError from "../error/baseError";
+import APIError from "../error/apiError";
 import NotFoundError from "../error/notfoundError";
 import BadRequestError from "../error/badrequestError";
-import InternalServerError from "../error/internalserverError";
 import globalErrorHandler from "../middleware/globalErrorHandlingMiddleware.ts";
 
 const router = Router();
@@ -14,27 +13,33 @@ router.use("/v1/tours", routerV1);
 router.use("/v2/tours", routerV2);
 
 router.all("*", (req: Request, res: Response, next: NextFunction) => {
-  throw new NotFoundError(
-    `No resource or route defined for ${req.originalUrl}`
+  next(
+    new NotFoundError(`No resource or route defined for ${req.originalUrl}`)
   );
 });
 
 router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof SyntaxError) {
-    throw new BadRequestError("Bad JSON");
+    next(new BadRequestError("Bad JSON"));
   }
+
+  next(err);
 });
 
-router.use(
-  (err: BaseError, req: Request, res: Response, next: NextFunction) => {
-    if (globalErrorHandler.isTrustedError(err)) {
-      return res
-        .status(err.errorCode)
-        .json({ error: err.name, message: err.message });
-    }
-
-    globalErrorHandler.handleAPIError(err);
+router.use((err: APIError, req: Request, res: Response, next: NextFunction) => {
+  if (globalErrorHandler.isTrustedError(err)) {
+    return res
+      .status(err.httpStatusCode)
+      .json({ error: err.name, message: err.message });
   }
-);
+
+  globalErrorHandler.handleAPIError(err);
+
+  res.status(500).json({
+    error: "INTERNAL SERVER ERROR",
+    message:
+      "Oops! Sorry an error occured on our end, we cannot process your request at this time. Please try again shortly.",
+  });
+});
 
 export default router;
