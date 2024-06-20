605e7bca3fe51a1036dfca4b0aa35d685e91f763
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-20 09:15:11 +0100

    fix: implement new route handlers

--- tours/src/router/v1/tourRouter.ts
+++ tours/src/router/v1/tourRouter.ts
@@ -3,12 +3,17 @@ import tourController from "../../controller/v1/tourController";
 
 const routerV1 = Router();
 
-routerV1.route("/search").get(tourController.retrieveToursSearch);
+routerV1.route("/").get(tourController.retrieveTourCollection);
+
+routerV1.route("/search").get(tourController.retrieveTourSearch);
 
 routerV1
   .route("/:id")
-  .put(tourController.replaceTour)
-  .patch(tourController.updateTour)
-  .delete(tourController.cancelTour);
+  .get(tourController.retrieveTourItem)
+  .put(tourController.replaceTourItem)
+  .patch(tourController.updateTourItem)
+  .patch(tourController.completeTourItem)
+  .patch(tourController.cancelTourItem)
+  .patch(tourController.reopenTourItem);
 
 export default routerV1;
