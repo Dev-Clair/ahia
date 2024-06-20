e57c17c79a4e11d8c2383a9be8277bc660c6b6ea
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-20 22:41:29 +0100

    fix: handler

--- tours/handler.ts
+++ tours/handler.ts
@@ -2,35 +2,38 @@ import serverless from "serverless-http";
 import App from "./api/app";
 import Config from "./config";
 import Connection from "./connection";
-import tourCommand from "./event/tourCommand";
+import tourCommand from "./event/command/tourCommand";
 
 Connection(Config.MONGO_URI);
 
-if (Config.NODE_ENV === "development") {
-  App.listen(Config.SERVER_PORT, () => {
-    console.log(
-      `Server process started, listening on port ${Config.SERVER_PORT}`
-    );
-  });
-} else {
-  // const tourAPI = serverless(App);
-  // const tour = async (event: any, context: any) => {
-  //   if (event.source && event["detail-type"]) {
-  //     let payload;
-  //     payload = JSON.parse(event.detail);
-  //     const { fullDocument: paymentDetails } = payload;
-  //     const { customerId: customerId, listingsId: listingIds } = paymentDetails;
-  //     const command: {
-  //       customerId: string;
-  //       listingIds: string[];
-  //     } = {
-  //       customerId: customerId,
-  //       listingIds: listingIds,
-  //     };
-  //     await tourCommand(command);
-  //   } else {
-  //     return tourAPI(event, context);
-  //   }
-  // };
-  // export default tour;
-}
+const tourAPI = serverless(App);
+
+const tour = async (event: any, context: any) => {
+  try {
+    if (event.source && event["detail-type"]) {
+      let payload;
+
+      payload = JSON.parse(event.detail);
+
+      const { fullDocument: paymentDetails } = payload;
+
+      const { customerId: customerId, listingsId: listingIds } = paymentDetails;
+
+      const command: {
+        customerId: string;
+        listingIds: string[];
+      } = {
+        customerId: customerId,
+        listingIds: listingIds,
+      };
+
+      await tourCommand(command);
+    } else {
+      return tourAPI(event, context);
+    }
+  } catch (err: any) {
+    console.error(err);
+  }
+};
+
+export default tour;
