f589d207729fa2d945debfa2a47fe73169d0457b
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-20 20:52:52 +0100

    feat: destructure model from schema

--- tours/src/model/tourModel.ts
+++ tours/src/model/tourModel.ts
@@ -1,39 +1,6 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
+import TourSchema from "../schema/tourSchema";
 import TourInterface from "../interface/tourInterface";
-
-const TourSchema: Schema<TourInterface> = new Schema({
-  realtorId: {
-    type: String,
-    required: false,
-  },
-  customerId: {
-    type: String,
-    required: [true, "A new tour must have a customerId"],
-  },
-  listingIds: [
-    {
-      type: String,
-      required: [true, "A new tour must have a a collection of listings"],
-    },
-  ],
-  scheduledDate: {
-    type: Date,
-    required: false,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "completed", "cancelled"],
-    default: "pending",
-  },
-  isClosed: {
-    type: Boolean,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
 
 const Tour = mongoose.model<TourInterface>("Tour", TourSchema);
 
---
+++ tours/src/schema/tourSchema.ts
@@ -0,0 +1,38 @@
+import { Schema } from "mongoose";
+import TourInterface from "../interface/tourInterface";
+
+const TourSchema: Schema<TourInterface> = new Schema({
+  realtorId: {
+    type: String,
+    required: false,
+  },
+  customerId: {
+    type: String,
+    required: [true, "A new tour must have a customerId"],
+  },
+  listingIds: [
+    {
+      type: String,
+      required: [true, "A new tour must have a a collection of listings"],
+    },
+  ],
+  scheduledDate: {
+    type: Date,
+    required: false,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "completed", "cancelled"],
+    default: "pending",
+  },
+  isClosed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default TourSchema;
