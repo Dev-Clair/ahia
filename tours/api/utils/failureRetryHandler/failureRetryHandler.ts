e3ca81c4d6a12987a2ea639280e73719bf1fe2c4
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-24 20:58:47 +0100

    feat: implement retry handling logic

--- tours/api/utils/failureRetryHandler/failureRetryHandler.ts
+++ tours/api/utils/failureRetryHandler/failureRetryHandler.ts
@@ -1,2 +1,54 @@
 import pRetry from "p-retry";
-import retry from "async-retry";
+import asyncRetry from "async-retry";
+
+const exponentialRetry = async (
+  operation: Function,
+  retries: number,
+  factor: number,
+  minTimeout: number
+) => {
+  await pRetry(
+    async () => {
+      await operation();
+    },
+    {
+      retries: retries,
+      factor: factor,
+      minTimeout: minTimeout,
+      onFailedAttempt: (err) => {
+        console.log(
+          `Exponential retry attempt ${err.attemptNumber} failed. There are ${err.retriesLeft} retries left. Error: ${err.message}`
+        );
+      },
+    }
+  );
+};
+
+const linearJitterRetry = async (
+  operation: Function,
+  retries: number,
+  minTimeout: number,
+  jitterFactor: number
+) => {
+  await asyncRetry(
+    async () => {
+      await operation();
+    },
+    {
+      retries: retries,
+      minTimeout: minTimeout,
+      onRetry: (err, attempt) => {
+        const jitter = Math.random() * jitterFactor;
+
+        console.log(
+          `Linear jitter retry attempt ${attempt} failed. Error: ${
+            err.message
+          }. Next retry in ${5000 + jitter}ms`
+        );
+        return 5000 + jitter;
+      },
+    }
+  );
+};
+
+export default { exponentialRetry, linearJitterRetry };
