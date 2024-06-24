b0282e54b3bc8664bbdb75013e0a96abc19f0c43
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-24 15:22:29 +0100

    fix: define new routes and handlers

--- tours/api/router/v1/tourRouter.ts
+++ tours/api/router/v1/tourRouter.ts
@@ -10,10 +10,15 @@ TourRouterV1.route("/search").get(tourController.retrieveTourSearch);
 TourRouterV1.route("/:id")
   .get(tourController.retrieveTourItem)
   .put(tourController.replaceTourItem)
-  .patch(tourController.updateTourItem)
-  .patch(tourController.completeTourItem)
-  .patch(tourController.cancelTourItem)
-  .patch(tourController.reopenTourItem);
+  .patch(tourController.updateTourItem);
+
+TourRouterV1.route("/:id/status/complete").patch(
+  tourController.completeTourItem
+);
+
+TourRouterV1.route("/:id/status/cancel").patch(tourController.cancelTourItem);
+
+TourRouterV1.route("/:id/status/reopen").patch(tourController.reopenTourItem);
 
 TourRouterV1.route(":/id/schedule").post(tourController.scheduleTour);
 
