49be51d376396628fdbf8be4a4c8668e63c1c636
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-25 23:06:23 +0100

    fix: modify function parameter type

--- tours/api/utils/notificationHandler/notificationHandler.ts
+++ tours/api/utils/notificationHandler/notificationHandler.ts
@@ -3,25 +3,25 @@ import SendEmail from "../../service/mailService";
 const notifyAdmin = async (
   subject: String,
   body: String,
-  toAddresses: String,
-  ccAddresses?: String,
-  bccAddresses?: String
+  toAddresses: [String],
+  ccAddresses?: [String],
+  bccAddresses?: [String]
 ) => {};
 
 const notifyRealtor = async (
   subject: String,
   body: String,
-  toAddresses: String,
-  ccAddresses?: String,
-  bccAddresses?: String
+  toAddresses: [String],
+  ccAddresses?: [String],
+  bccAddresses?: [String]
 ) => {};
 
 const notifyCustomer = async (
   subject: String,
   body: String,
-  toAddresses: String,
-  ccAddresses?: String,
-  bccAddresses?: String
+  toAddresses: [String],
+  ccAddresses?: [String],
+  bccAddresses?: [String]
 ) => {};
 
 export default { notifyAdmin, notifyRealtor, notifyCustomer };
