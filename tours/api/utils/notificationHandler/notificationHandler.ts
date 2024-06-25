b8f1debcceb53a4b8695aa71f467d7be85c7f47b
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-25 20:17:40 +0100

    feat: implement notification handler

---
+++ tours/api/utils/notificationHandler/notificationHandler.ts
@@ -0,0 +1,9 @@
+import SendEmail from "../../../src/service/mailService";
+
+const notifyAdmin = async () => {};
+
+const notifyRealtor = async () => {};
+
+const notifyCustomer = async () => {};
+
+export default { notifyAdmin, notifyRealtor, notifyCustomer };
