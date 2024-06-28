7a0dc165b2410bf977d34168e9812d517698824a
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-28 20:43:47 +0100

    feat: load new env variables

--- tourAPI/config.ts
+++ tourAPI/config.ts
@@ -7,6 +7,7 @@ const Config = {
   NODE_ENV: process.env.NODE_ENV || "",
   SERVER_PORT: process.env.SERVER_PORT || 5999,
   MONGO_URI: process.env.MONGO_URI || "",
+  TOUR_ADMIN_EMAIL: process.env.TOUR_ADMIN_EMAIL || "",
   AWS_REGION: process.env.AWS_REGION || "",
   AWS_ACCOUNT_ID: process.env.AWS_ACCOUNT_ID || "",
 };
