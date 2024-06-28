8c586369bb41eb8f6751c1b4d9613e5bc5ded0b1
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-06-28 16:35:12 +0100

    feat: properly handle graceful exit

--- tours/index.ts
+++ tours/index.ts
@@ -1,7 +1,8 @@
 import cluster from "cluster";
 import os from "os";
+import mongoose from "mongoose";
 import App from "./app";
-import Cron from "./cron/cron";
+import Cron from "./cron";
 import Config from "./config";
 import Connection from "./connection";
 import Logger from "./api/service/loggerService";
@@ -9,63 +10,52 @@ import Logger from "./api/service/loggerService";
 const numCPUs = os.cpus().length;
 
 if (cluster.isPrimary) {
-  Logger.info(`Master ${process.pid} is running`);
-
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
   cluster.on("exit", (worker, code, signal) => {
-    Logger.info(`Worker ${worker.process.pid} died. Restarting...`);
+    Logger.info(
+      `worker process ${worker.process.pid} exits with code: ${code}, signal: ${signal}`
+    );
+
     cluster.fork();
   });
 } else {
   Connection(Config.MONGO_URI);
 
-  const server = App.listen(Config.SERVER_PORT, () => {
-    Logger.info(
-      `Worker ${process.pid} started, listening on port ${Config.SERVER_PORT}`
-    );
-  });
+  const server = App.listen(Config.SERVER_PORT, () =>
+    Logger.info(`server up and listening on port ${Config.SERVER_PORT}`)
+  );
 
-  // Cron.start();
-
-  process.on("unhandledRejection", (reason, promise) => {
-    Logger.error("Unhandled Rejection at:", promise, "reason:", reason);
-    process.exitCode = 1;
-  });
+  Cron.start();
 
   process.on("uncaughtException", (error) => {
     Logger.error("Uncaught Exception thrown:", error);
     process.exitCode = 1;
   });
 
+  process.on("unhandledRejection", (reason, promise) => {
+    Logger.error("Unhandled Rejection at:", promise, "reason:", reason);
+    process.exitCode = 1;
+  });
+
   const shutdown = () => {
     Logger.info("Shutting down gracefully...");
-    server.close(() => {
-      Logger.info("Closed out remaining connections");
-      process.exitCode = 1;
-    });
 
-    setTimeout(() => {
-      Logger.info("Forcing server shutdown");
-      process.exitCode = 1;
-    }, 10000);
+    // Stop running cron task
+    Cron.stop();
+
+    // Close open mongoose connection
+    mongoose.connection.close(true);
+
+    // Close server process
+    server.close(() => {
+      Logger.info("Closed out remaining connections, initiating shutdown");
+      process.kill(process.pid, "SIGTERM");
+    });
   };
 
-  process.on("SIGTERM", shutdown);
   process.on("SIGINT", shutdown);
+  process.on("SIGTERM", shutdown);
 }
-
-// prod: tslib
-// "scripts": {
-//   "build": "tsc",
-//   "start": "node dist/index.js",
-//   "start:dev": "nodemon src/index.ts"
-// },
-
-// dev: tsc
-// "scripts": {
-//   "build": "tsc",
-//   "start": "node dist/index.js"
-// },
