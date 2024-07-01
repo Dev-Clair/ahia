9f7037ee160f9dcee1ba228c0e15c947ce7233a1
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-01 12:09:26 +0100

    fix: remove undefined type

--- tourAPI/src/interface/tourInterface.ts
+++ tourAPI/src/interface/tourInterface.ts
@@ -1,16 +1,16 @@
 import { Document } from "mongoose";
 
 export default interface TourInterface extends Document {
-  realtorId?: string;
+  realtorId: string;
   customerId: string;
   listingIds: string[];
   // location: {
   //   type?: String;
   //   coordinates?: Number;
   // };
-  scheduledDate?: Date;
-  scheduledTime?: string;
+  scheduledDate: Date;
+  scheduledTime: string;
   status: "pending" | "ongoing" | "completed" | "cancelled";
-  isClosed?: boolean;
+  isClosed: boolean;
   createdAt: Date;
 }
