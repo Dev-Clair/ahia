4d04be6377291ef2057c268ffbfd3e848cc7575d
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-02 09:03:05 +0100

    fix: refactor global error middleware handler

--- tourAPI/src/middleware/globalErrorHandlingMiddleware.ts.ts
+++ tourAPI/src/middleware/globalErrorHandlingMiddleware.ts.ts
@@ -5,33 +5,23 @@ import NotifyUser from "../utils/notificationHandler/notificationHandler";
 
 class GlobalErrorHandlingMiddleware {
   public async handleError(err: Error): Promise<void> {
-    Logger.error(`name: ${err.name}\n message: ${err.message}`);
+    console.log(err.name, err.message, err.stack);
+
+    // Logger.error(`name: ${err.name}\n message: ${err.message}`);
 
     // await NotifyUser();
   }
 
-  public isSyntaxError(err: Error): Boolean {
-    if (err instanceof SyntaxError) {
-      return true;
-    }
-
-    return false;
+  public isTrustedError(err: Error): boolean {
+    return err instanceof APIError && err.isOperational;
   }
 
-  public isSafeError(err: Error): Boolean {
-    if (err instanceof MongooseError) {
-      return true;
-    }
-
-    return false;
+  public isSafeError(err: Error): boolean {
+    return err instanceof MongooseError;
   }
 
-  public isTrustedError(err: Error): Boolean {
-    if (err instanceof APIError) {
-      return err.isOperational;
-    }
-
-    return false;
+  public isSyntaxError(err: Error): boolean {
+    return err instanceof SyntaxError && err.message.includes("JSON");
   }
 }
 
