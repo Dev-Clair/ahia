5b42bbd675bbdb70871d25e051453935ca8cf7e0
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-02 11:49:58 +0100

    fix: implement new middleware handlers

--- tourAPI/src/middleware/v1/tourMiddleWare.ts
+++ tourAPI/src/middleware/v1/tourMiddleWare.ts
@@ -1,25 +1,69 @@
 import { NextFunction, Request, Response } from "express";
 import HttpStatusCode from "../../enum/httpStatusCode";
-import BadRequestError from "../../error/badrequestError";
 
 /**
- * Check for idempotency key in request headers
+ * Check request protocol
+ */
+const checkRequestProtocol = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const getProtocol = req.protocol;
+
+  const getSecurity = req.secure;
+
+  if (getProtocol !== "https" || getSecurity === false) {
+    return res.status(HttpStatusCode.FORBIDDEN).json({
+      message: "Connection is not secure. SSL required",
+    });
+  }
+
+  next();
+};
+
+/**
+ * Check idempotency key in request headers
  */
 const checkIdempotencyKey = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const idempotencyKey = req.headers["idempotency-key"] as string;
+  const getIdempotencyKey = req.headers["idempotency-key"] as string;
 
-  if (!idempotencyKey) {
-    throw new BadRequestError(
-      HttpStatusCode.BAD_REQUEST,
-      `Idempotency key is required`
-    );
+  if (!getIdempotencyKey) {
+    return res.status(HttpStatusCode.BAD_REQUEST).json({
+      message: "Idempotency key is required",
+    });
   }
 
   next();
 };
 
-export default { checkIdempotencyKey };
+/**
+ * Check for request content type
+ */
+const checkRequestContentType = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const getContentType = req.headers["Content-Type"] as string;
+
+  if (getContentType !== "application/json") {
+    return res.status(HttpStatusCode.BAD_REQUEST).json({
+      message: "Invalid content type",
+      expected: "application/json",
+      received: `${getContentType}`,
+    });
+  }
+
+  next();
+};
+
+export default {
+  checkRequestProtocol,
+  checkIdempotencyKey,
+  checkRequestContentType,
+};
