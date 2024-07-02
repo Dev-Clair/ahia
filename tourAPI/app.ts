0e4bcfdcab92ff234c5b37e0c0c0fc65c851f9ef
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-02 15:59:31 +0100

    fix: move middleware from router to app

--- tourAPI/app.ts
+++ tourAPI/app.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import express, { Request, Response, Router, NextFunction } from "express";
 import helmet from "helmet";
 import hpp from "hpp";
 import express_mongo_sanitize from "express-mongo-sanitize";
 import TourRouter from "./src/router";
+import HttpStatusCode from "./src/enum/httpStatusCode";
+import APIError from "./src/error/apiError";
+import GlobalErrorHandler from "./src/middleware/globalErrorHandlingMiddleware.ts";
 
 const App = express();
 
@@ -17,5 +20,57 @@ App.use(hpp());
 App.use(express_mongo_sanitize());
 
 App.use("/api", TourRouter);
+
+App.all("*", (req: Request, res: Response, next: NextFunction) => {
+  return res
+    .status(HttpStatusCode.NOT_FOUND)
+    .json({ message: `No resource or route defined for ${req.originalUrl}` });
+});
+
+App.use(
+  (err: APIError | Error, req: Request, res: Response, next: NextFunction) => {
+    if (GlobalErrorHandler.isSyntaxError(err)) {
+      return res
+        .status(HttpStatusCode.BAD_REQUEST)
+        .json({ error: err.name, message: "Bad or Malformed JSON" });
+    }
+
+    if (GlobalErrorHandler.isSafeError(err)) {
+      if (err.name === "ValidationError") {
+        return res.status(HttpStatusCode.UNPROCESSABLE_ENTITY).json({
+          error: err.name,
+          message: "Invalid data provided",
+        });
+      } else if (err.name === "CastError") {
+        return res.status(HttpStatusCode.UNPROCESSABLE_ENTITY).json({
+          error: err.name,
+          message: "Invalid ID format",
+        });
+      } else {
+        return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+          error: err.name,
+          message:
+            "Oops! Sorry an error occured on our end, we cannot process your request at this time. Please try again shortly",
+        });
+      }
+    }
+
+    if (GlobalErrorHandler.isTrustedError(err as APIError)) {
+      return res
+        .status((err as APIError).httpStatusCode)
+        .json({ error: err.name, message: err.message });
+    }
+
+    GlobalErrorHandler.handleError(err);
+
+    if (!res.headersSent) {
+      return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+        error: "INTERNAL SERVER ERROR",
+        message:
+          "Oops! Sorry an error occured on our end, we cannot process your request at this time. Please try again shortly",
+      });
+    }
+  }
+);
 
 export default App;
--- tourAPI/src/router/index.ts
+++ tourAPI/src/router/index.ts
@@ -1,66 +1,11 @@
-import { Request, Response, Router, NextFunction } from "express";
+import { Router } from "express";
 import TourRouterV1 from "./v1/tourRouter";
 import TourRouterV2 from "./v2/tourRouter";
-import HttpStatusCode from "../enum/httpStatusCode";
-import APIError from "../error/apiError";
-import GlobalErrorHandler from "../middleware/globalErrorHandlingMiddleware.ts";
 
 const TourRouter = Router();
 
 TourRouter.use("/v1/tours", TourRouterV1);
 
 TourRouter.use("/v2/tours", TourRouterV2);
-
-TourRouter.all("*", (req: Request, res: Response, next: NextFunction) => {
-  return res
-    .status(HttpStatusCode.NOT_FOUND)
-    .json({ message: `No resource or route defined for ${req.originalUrl}` });
-});
-
-TourRouter.use(
-  (err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (res.headersSent) {
-      return next(err);
-    }
-
-    if (GlobalErrorHandler.isSyntaxError(err)) {
-      return res
-        .status(HttpStatusCode.BAD_REQUEST)
-        .json({ error: err.name, message: "Bad or Malformed JSON" });
-    }
-
-    if (GlobalErrorHandler.isSafeError(err)) {
-      if (err.name === "ValidationError" || err.name === "CastError") {
-        return res.status(HttpStatusCode.UNPROCESSABLE_ENTITY).json({
-          error: err.name,
-          message: JSON.stringify(err.message),
-        });
-      } else {
-        return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
-          error: err.name,
-          message: err.message,
-        });
-      }
-    }
-  }
-);
-
-TourRouter.use(
-  (err: APIError, req: Request, res: Response, next: NextFunction) => {
-    if (GlobalErrorHandler.isTrustedError(err)) {
-      return res
-        .status(err.httpStatusCode)
-        .json({ error: err.name, message: err.message });
-    }
-
-    GlobalErrorHandler.handleError(err);
-
-    return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
-      error: "INTERNAL_SERVER_ERROR",
-      message:
-        "Oops! Sorry an error occured on our end, we cannot process your request at this time. Please try again shortly.",
-    });
-  }
-);
 
 export default TourRouter;
