4a4c3c45a580156968e03a94a4727d4820204ae1
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-02 16:17:55 +0100

    fix: configure httpserver security based on environment

--- tourAPI/index.ts
+++ tourAPI/index.ts
@@ -1,26 +1,9 @@
 import http from "./http";
 import mongoose from "mongoose";
 import App from "./app";
-import Cron from "./cron";
 import Config from "./config";
 import Connection from "./connection";
 import Logger from "./src/service/loggerService";
-
-const HTTP = http.HTTP(App);
-
-// const HTTPS = http.HTTPS(App);
-
-HTTP.listen(Config.HTTP_PORT, () => {
-  Logger.info(`Listening on port ${Config.HTTP_PORT}`);
-});
-
-// HTTPS.listen(Config.HTTPS_PORT, () => {
-//   console.log(`Listening on port ${Config.HTTPS_PORT}`);
-// });
-
-Connection(Config.MONGO_URI);
-
-Cron.start();
 
 process.on("uncaughtException", (error) => {
   Logger.error("Uncaught Exception thrown:", error);
@@ -32,24 +15,46 @@ process.on("unhandledRejection", (reason, promise) => {
   process.exitCode = 1;
 });
 
+const HTTP = http.HTTP(App);
+
+// const HTTPS = http.HTTPS(App);
+
+if (Config.NODE_ENV === "development") {
+  HTTP.listen(Config.HTTP_PORT, () => {
+    Logger.info(`Listening on port ${Config.HTTP_PORT}`);
+  });
+}
+
+// if (Config.NODE_ENV === "production") {
+//   HTTPS.listen(Config.HTTPS_PORT, () => {
+//     console.log(`Listening on port ${Config.HTTPS_PORT}`);
+//   });
+// }
+
+Connection(Config.MONGO_URI);
+
 const shutdown = () => {
   Logger.info("Shutting down gracefully...");
 
-  // Stop running cron tasks
-  Cron.stop();
-
-  // Close open mongoose connections
+  // Close open database connections
   mongoose.connection.close(true);
 
   // Close running server process
+  if (Config.NODE_ENV === "development") {
+    Logger.info(
+      `Closing all connections to server on port ${Config.HTTP_PORT}`
+    );
 
-  Logger.info(`Closing all connections to server on port ${Config.HTTP_PORT}`);
+    HTTP.closeAllConnections();
+  }
 
-  HTTP.closeAllConnections();
+  // if (Config.NODE_ENV === "production") {
+  //   Logger.info(
+  //     `Closing all connections to server on port ${Config.HTTPS_PORT}`
+  //   );
 
-  // Logger.info(`Closing all connections to server on port ${Config.HTTPS_PORT}`);
-
-  // HTTPS.closeAllConnections();
+  //   HTTPS.closeAllConnections();
+  // }
 
   process.on("SIGINT", shutdown);
   process.on("SIGTERM", shutdown);
