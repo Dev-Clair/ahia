1eac1f50984e52beef597cb46390caa5d61fd99d
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-07 10:51:37 +0100

    fix: encapsulate retry logic ointo a class

--- tourCron/retry.js
+++ tourCron/retry.js
@@ -1,71 +1,98 @@
 const asyncRetry = require("async-retry");
 
-const ExponentialRetry = async (
-  operation,
-  options = { retries: 3, factor: 2, minTimeout: 7500 }
-) => {
-  return asyncRetry.default(
-    async (bail, attempt) => {
-      return await operation();
-    },
-    {
-      retries: options.retries,
-      factor: options.factor,
-      minTimeout: options.minTimeout,
-      onRetry: (error, attempt) => {
-        console.error.error(
-          `Exponential retry attempt no. ${attempt} failed.\nThere are ${
-            options.retries - attempt
-          } retries left.\nError: ${error.message}`
-        );
+class Retry {
+  static async ExponentialBackoff(
+    operation,
+    options = { retries: 3, factor: 2, minTimeout: 7500 }
+  ) {
+    return asyncRetry.default(
+      async (bail, attempt) => {
+        return await operation();
       },
-    }
-  );
-};
+      {
+        retries: options.retries,
+        factor: options.factor,
+        minTimeout: options.minTimeout,
+        onRetry: (error, attempt) => {
+          console.error.error(
+            `Exponential retry attempt no. ${attempt} failed.\nThere are ${
+              options.retries - attempt
+            } retries left.\nError: ${error.message}`
+          );
+        },
+      }
+    );
+  }
 
-const LinearJitterRetry = async (
-  operation,
-  options = { retries: 2, minTimeout: 5000, jitterFactor: 1000 }
-) => {
-  return asyncRetry.default(
-    async (bail, attempt) => {
-      return await operation();
-    },
-    {
-      retries: options.retries,
-      minTimeout: options.minTimeout,
-      onRetry: (error, attempt) => {
-        const jitter = Math.random() * options.jitterFactor;
-        console.error(
-          `Linear jitter retry attempt no. ${attempt} failed.\nError: ${
-            error.message
-          }.\nNext retry in ${options.minTimeout + jitter}ms`
-        );
-        return options.minTimeout + jitter;
+  static async ExponentialJitterBackoff(
+    operation,
+    options = { retries: 3, factor: 2, minTimeout: 7500, jitterFactor: 1000 }
+  ) {
+    return asyncRetry.default(
+      async (bail, attempt) => {
+        return await operation();
       },
-    }
-  );
-};
+      {
+        retries: options.retries,
+        factor: options.factor,
+        minTimeout: options.minTimeout,
+        onRetry: (error, attempt) => {
+          const jitter = Math.random() * options.jitterFactor;
+          const timeout =
+            options.minTimeout * Math.pow(options.factor, attempt - 1) + jitter;
+          console.error(
+            `Exponential jitter retry attempt no. ${attempt} failed.\nError: ${error.message}.\nNext retry in ${timeout}ms`
+          );
+          return timeout;
+        },
+      }
+    );
+  }
 
-const LinearRetry = async (
-  operation,
-  options = { retries: 5, minTimeout: 7500 }
-) => {
-  return asyncRetry.default(
-    async (bail, attempt) => {
-      return await operation();
-    },
-    {
-      retries: options.retries,
-      minTimeout: options.minTimeout,
-      onRetry: (error, attempt) => {
-        console.error(
-          `Linear retry attempt no. ${attempt} failed.\nError: ${error.message}.\nNext retry in ${options.minTimeout}ms`
-        );
-        return options.minTimeout;
+  static async LinearBackoff(
+    operation,
+    options = { retries: 5, minTimeout: 7500 }
+  ) {
+    return asyncRetry.default(
+      async (bail, attempt) => {
+        return await operation();
       },
-    }
-  );
-};
+      {
+        retries: options.retries,
+        minTimeout: options.minTimeout,
+        onRetry: (error, attempt) => {
+          console.error(
+            `Linear retry attempt no. ${attempt} failed.\nError: ${error.message}.\nNext retry in ${options.minTimeout}ms`
+          );
+          return options.minTimeout;
+        },
+      }
+    );
+  }
 
-module.exports = { ExponentialRetry, LinearJitterRetry, LinearRetry };
+  static async LinearJitterBackoff(
+    operation,
+    options = { retries: 2, minTimeout: 5000, jitterFactor: 1000 }
+  ) {
+    return asyncRetry.default(
+      async (bail, attempt) => {
+        return await operation();
+      },
+      {
+        retries: options.retries,
+        minTimeout: options.minTimeout,
+        onRetry: (error, attempt) => {
+          const jitter = Math.random() * options.jitterFactor;
+          console.error(
+            `Linear jitter retry attempt no. ${attempt} failed.\nError: ${
+              error.message
+            }.\nNext retry in ${options.minTimeout + jitter}ms`
+          );
+          return options.minTimeout + jitter;
+        },
+      }
+    );
+  }
+}
+
+module.exports = Retry;
