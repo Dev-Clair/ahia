31f8df2cd77e3fb1e315da2a66a93176d544aa9d
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-21 23:19:21 +0100

    fix: modify link

--- tourCron/tourNotificationManager.js
+++ tourCron/tourNotificationManager.js
@@ -41,7 +41,7 @@ class TourNotificationManager {
         `https://www.ahia.com/api/v1/tours/${tourId}/reschedule`
       );
 
-      const text = `You have a scheduled tour on ${tourDate.toDateString()} at ${tourTime}.\n\nTo reschedule, kindly click the ${link} to initiate the rescheduling process.\nPlease note that rescheduling a tour is subject to the tour party approval and automatically becomes impossible 3-Hrs before the original scheduled time.`;
+      const text = `You have a scheduled tour on ${tourDate.toDateString()} at ${tourTime}.\n\nTo reschedule, kindly click the ${link} to initiate the rescheduling process.\nPlease note that rescheduling a tour is subject to the tour party approval and you might incur a penalty fee if initiated 3-Hrs before the scheduled time.`;
 
       await Mail(sender, [customerEmail, realtorEmail], subject, text);
 
