d9a2567ec94ebb068b1e26fa4bfa24fe45ccbb52
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-25 15:30:37 +0100

    feat: create middleware

---
+++ iam/src/middleware/globalErrorHandlingMiddleware.ts.ts
@@ -0,0 +1,40 @@
+import { MongooseError } from "mongoose";
+import APIError from "../error/apiError";
+import Config from "../../config";
+import Logger from "../service/loggerService";
+import Mail from "../utils/mail";
+
+class GlobalErrorHandlingMiddleware {
+  public static async handleError(err: Error): Promise<void> {
+    Logger.error(
+      `name: ${err.name}\nmessage: ${err.message}\nstack: ${err.stack}`
+    );
+
+    const sender: string = Config.LISTING_ADMIN_EMAIL_I;
+
+    const recipient: [string] = [Config.LISTING_ADMIN_EMAIL_II];
+
+    await Mail(
+      sender,
+      recipient,
+      err.name,
+      JSON.stringify({ message: err.message, stack: err.stack })
+    );
+  }
+
+  public static isTrustedError(err: APIError | Error): boolean {
+    return err instanceof APIError && err.isOperational;
+  }
+
+  public static isSafeError(err: Error): boolean {
+    return err instanceof MongooseError;
+  }
+
+  public static isSyntaxError(err: Error): boolean {
+    return err instanceof SyntaxError && err.message.includes("JSON");
+  }
+}
+
+const GlobalErrorHandler = GlobalErrorHandlingMiddleware;
+
+export default GlobalErrorHandler;
