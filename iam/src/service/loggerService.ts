5a0067218975104eb804c4d7baa3bf114b91a594
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-25 15:32:10 +0100

    feat: create services

---
+++ iam/src/service/loggerService.ts
@@ -0,0 +1,24 @@
+import { createLogger, format, transports } from "winston";
+
+const { combine, timestamp, printf, colorize } = format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+const Logger = createLogger({
+  level: "info",
+  format: combine(timestamp(), logFormat),
+  exceptionHandlers: [new transports.File({ filename: "logs/exceptions.log" })],
+  rejectionHandlers: [new transports.File({ filename: "logs/rejections.log" })],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  Logger.add(
+    new transports.Console({
+      format: combine(colorize(), logFormat),
+    })
+  );
+}
+
+export default Logger;
