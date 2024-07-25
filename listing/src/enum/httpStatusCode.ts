1d23923a1503f4717029eaecd8da2281e87174ac
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-25 12:01:13 +0100

    feat: define http status code enum

---
+++ listing/src/enum/httpStatusCode.ts
@@ -0,0 +1,14 @@
+enum HttpStatusCode {
+  OK = 200,
+  CREATED = 201,
+  MODIFIED = 204,
+  BAD_REQUEST = 400,
+  UNAUTHORISED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  METHOD_NOT_ALLOWED = 405,
+  UNPROCESSABLE_ENTITY = 422,
+  INTERNAL_SERVER_ERROR = 500,
+}
+
+export default HttpStatusCode;
