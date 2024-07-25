4b88dfcab1116348ef1e1bceae7702296b178b32
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-25 09:50:32 +0100

    fix: modify config setup

--- listing/config.ts
+++ listing/config.ts
@@ -1,14 +1,22 @@
 import process from "process";
 import dotenv from "dotenv";
 
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 const Config = {
-  NODE_ENV: process.env.NODE_ENV || "",
-  SERVER_PORT: process.env.SERVER_PORT || 3999,
+  AWS_REGION: process.env.AWS_REGION,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  HTTP_PORT: process.env.HTTP_PORT || 4999,
+  HTTPS_PORT: process.env.HTTPS_PORT || 5000,
   MONGO_URI: process.env.MONGO_URI || "",
-  AWS_REGION: process.env.AWS_REGION || "",
-  AWS_ACCOUNT_ID: process.env.AWS_ACCOUNT_ID || "",
+  NODE_ENV: process.env.NODE_ENV || "test",
+  SSL_KEY_FILE_PATH: process.env.SSL_KEY_FILE_PATH || "",
+  SSL_CERT_FILE_PATH: process.env.SSL_CERT_FILE_PATH || "",
+  LISTING_ADMIN_EMAIL_I: process.env.LISTING_ADMIN_EMAIL_I || "",
+  LISTING_ADMIN_EMAIL_II: process.env.LISTING_ADMIN_EMAIL_II || "",
 };
 
 export default Config;
