0097803972d1ebd2e549875db04571ae2b8289a8
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-25 09:53:51 +0100

    feat: define utils scripts

---
+++ listing/src/utils/asynCatch.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+
+const AsyncCatch = (
+  Operation: (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => Promise<Response | void>,
+  Retry?: Function,
+  RetryOptions?: {}
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const executeOperation = () => Operation(req, res, next);
+
+    if (Retry) {
+      Retry(executeOperation, RetryOptions).catch(next);
+    } else {
+      executeOperation().catch(next);
+    }
+  };
+};
+
+export default AsyncCatch;
