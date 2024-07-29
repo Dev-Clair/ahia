b61e9805234e03b4a4286c338997e5bf73e80210
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-29 13:10:26 +0100

    fix: modify config vars

--- listingAPI/config.ts
+++ listingAPI/config.ts
@@ -6,15 +6,19 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const Config = {
-  AWS_REGION: process.env.AWS_REGION,
-  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  AWS: {
+    REGION: process.env.AWS_REGION || "af-south-1",
+    ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID || "",
+    SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || "",
+    S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME || "",
+    S3_FOLDER_NAME: process.env.AWS_S3_FOLDER_NAME || "",
+  },
   HTTP_PORT: process.env.HTTP_PORT || 4999,
   HTTPS_PORT: process.env.HTTPS_PORT || 5000,
   MONGO_URI: process.env.MONGO_URI || "",
   NODE_ENV: process.env.NODE_ENV || "test",
-  SERVICE_NAME: process.env.SERVICE_NAME,
-  SERVICE_SECRET: process.env.SERVICE_SECRET,
+  SERVICE_NAME: process.env.SERVICE_NAME || "",
+  SERVICE_SECRET: process.env.SERVICE_SECRET || "",
   SSL_KEY_FILE_PATH: process.env.SSL_KEY_FILE_PATH || "",
   SSL_CERT_FILE_PATH: process.env.SSL_CERT_FILE_PATH || "",
   LISTING_ADMIN_EMAIL_I: process.env.LISTING_ADMIN_EMAIL_I || "",
