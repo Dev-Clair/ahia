234077ddc6244fdebc69789f8d205e7d11b043e8
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-29 13:10:56 +0100

    fix: create service abstraction

--- listingAPI/src/service/storageService.ts
+++ listingAPI/src/service/storageService.ts
@@ -1,7 +1,30 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import {
+  DeleteObjectCommand,
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { nanoid } from "nanoid";
+import Config from "../../config";
 
-class AttachmentService {
-  constructor() {}
+class StorageService {
+  private s3: S3Client;
+
+  private bucket: string;
+  constructor(
+    s3: S3Client = new S3Client({ region: Config.AWS.REGION }),
+    bucket: string = Config.AWS.S3_BUCKET_NAME
+  ) {
+    this.s3 = s3;
+
+    this.bucket = bucket;
+  }
+
+  public async upload(): Promise<void> {}
+
+  public async download(): Promise<void> {}
+
+  public async remove(): Promise<void> {}
 }
 
-export default AttachmentService;
+export default StorageService;
