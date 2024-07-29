75603649f889438a28d965fe00a0bdf5e4421709
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-07-29 16:12:56 +0100

    fix: set up router

--- listingAPI/src/router/v1/listingRouter.ts
+++ listingAPI/src/router/v1/listingRouter.ts
@@ -1,5 +1,32 @@
 import { Router } from "express";
+import ListingController from "../../controller/v1/listingController";
+import ListingMiddleWare from "../../middleware/v1/listingMiddleWare";
+import ValidationMiddleware from "../../middleware/validationMiddleware";
 
 const ListingRouterV1 = Router();
+
+ListingRouterV1.route("/").get().post();
+
+ListingRouterV1.route("/search").get();
+
+ListingRouterV1.route("/:id")
+  .get()
+  .put(ListingController.operationNotAllowed)
+  .patch()
+  .delete();
+
+ListingRouterV1.route("/:id/attachments").post();
+
+ListingRouterV1.route("/:id/attachments/:attachmentId")
+  .get()
+  .put(ListingController.operationNotAllowed)
+  .delete();
+
+ListingRouterV1.route("/:id/promotions").post();
+
+ListingRouterV1.route("/:id/promotions/:promotionId")
+  .get()
+  .put(ListingController.operationNotAllowed)
+  .delete();
 
 export default ListingRouterV1;
