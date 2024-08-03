ff0652607f0638023b2f71af058aaa07f7f734b8
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-03 21:29:05 +0100

    feat: add methods

--- listingAPI/src/controller/attachmentController.ts
+++ listingAPI/src/controller/attachmentController.ts
@@ -1,52 +1,169 @@
+import { NextFunction, Request, Response } from "express";
 import Attachment from "../model/attachmentModel";
 import storageService from "../service/storageService";
+import Listing from "../model/listingModel";
+import Idempotency from "../model/idempotencyModel";
+import HttpStatusCode from "../enum/httpStatusCode";
+import NotFoundError from "../error/notfoundError";
+
+/***********************Helper Methods**************************************** */
+/**
+ * Ensures operation idempotency
+ * @param req
+ * @param res
+ * @returns Promise<Response | string>
+ */
+const ensureIdempotency = async (
+  req: Request,
+  res: Response
+): Promise<Response | string> => {
+  const idempotencyKey = (req.headers["idempotency-key"] as string) || "";
+
+  const verifyOperationIdempotency = await Idempotency.findOne({
+    key: idempotencyKey,
+  });
+
+  if (verifyOperationIdempotency) {
+    return res
+      .status(HttpStatusCode.CREATED)
+      .json({ data: verifyOperationIdempotency.response });
+  }
+
+  return idempotencyKey;
+};
 
 /**
- * Create a new attachment in collection.
+ * Handles not allowed operations
+ * @param req
+ * @param res
+ * @param next
+ * @returns Response
+ */
+const isNotAllowed = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Response => {
+  return res.status(HttpStatusCode.METHOD_NOT_ALLOWED).json({
+    data: {
+      message: "operation not allowed",
+    },
+  });
+};
+
+/***********************Attachment Business Logic**************************************** */
+
+/**
+ * Creates a new attachment resource in collection
+ * @param req
+ * @param res
+ * @param next
+ * @returns Promise<Response | void>
  */
 const createAttachmentCollection = async (
-  id: string,
-  type: string,
-  category: string,
-  body: any
-): Promise<string> => {
-  try {
-    const key = await storageService.upload(id, type, category, body);
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> => {
+  const idempotencyKey = await ensureIdempotency(req, res);
 
-    const attachment = await Attachment.create({
-      type: type,
-      category: category,
-      key: key,
-    });
+  const id = req.params.id;
 
-    return attachment.key;
-  } catch (err: any) {
-    throw err;
+  const { name, type, category, body } = req.body;
+
+  const listing = await Listing.findById({ _id: id });
+
+  if (!listing) {
+    throw new NotFoundError(
+      HttpStatusCode.NOT_FOUND,
+      `No listing found for id: ${req.params.id}`
+    );
   }
+
+  const key = await storageService.upload(id, type, category, body);
+
+  const attachment = await Attachment.create({
+    name: name,
+    type: type,
+    category: category,
+    key: key,
+  });
+
+  const response = {
+    data: {
+      message: `${attachment.name.toUpperCase()} successfully uploaded`,
+    },
+  };
+
+  await Idempotency.create({
+    key: idempotencyKey,
+    response: response,
+  });
+
+  return res.status(HttpStatusCode.CREATED).json(response);
 };
 
 /**
- * Retrieve collection of attachments.
+ * Retrieves collection of attachment resources
+ * @param req
+ * @param res
+ * @param next
+ * @returns Promise<Response | void>
  */
 const retrieveAttachmentCollection = async (
-  prefix: string
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<Response | void> => {
-  storageService.retrieveCollection(prefix);
+  const id = req.params.id;
+
+  const { key } = req.query;
+
+  const listing = await Listing.findById({ _id: id });
+
+  if (!listing) {
+    throw new NotFoundError(
+      HttpStatusCode.NOT_FOUND,
+      `No listing found for id: ${req.params.id}`
+    );
+  }
+
+  const attachments = await Attachment.find({
+    key: key,
+  });
+
+  const keys = await storageService.retrieveCollection(prefix);
+
+  return res.status(HttpStatusCode.OK).json({ data: attachments });
 };
 
 /**
- * Retrieve an attachment item using its :id.
+ * Retrieves an attachment resource from collection
+ * @param req
+ * @param res
+ * @param next
+ * @returns Promise<Response | void>
  */
 const retrieveAttachmentItem = async (
-  key: string
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<Response | void> => {
   storageService.download(key);
 };
 
 /**
- * Remove an attachment item using its :id.
+ * Removes an attachment resource from collection
+ * @param req
+ * @param res
+ * @param next
+ * @returns Promise<Response | void>
  */
-const deleteAttachmentItem = async (key: string): Promise<Response | void> => {
+const deleteAttachmentItem = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> => {
   storageService.remove(key);
 };
 
