b73e6f0368287f829ebfbcfa98a3263c632e70c9
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-04 10:18:23 +0100

    feat: create new util method

---
+++ listingAPI/src/utils/ensureIdempotency.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import HttpStatusCode from "../enum/httpStatusCode";
+import Idempotency from "../model/idempotencyModel";
+
+/**
+ * Ensures operation idempotency
+ * @param req
+ * @param res
+ * @returns Promise<Response | string>
+ */
+const EnsureIdempotency = async (
+  req: Request,
+  res: Response
+): Promise<Response | string> => {
+  const idempotencyKey = (req.headers["idempotency-key"] as string) || "";
+
+  const verifyOperationIdempotency = await Idempotency.findOne({
+    key: idempotencyKey,
+  });
+
+  if (verifyOperationIdempotency) {
+    return res
+      .status(HttpStatusCode.CREATED)
+      .json({ data: verifyOperationIdempotency.response });
+  }
+
+  return idempotencyKey;
+};
+
+export default EnsureIdempotency;
