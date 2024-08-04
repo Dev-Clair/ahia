6259d2032c29091c2a5bf311ad0fae3398ef0775
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-04 12:27:41 +0100

    fix: remove commented block

--- listingAPI/src/interface/attachmentInterface.ts
+++ listingAPI/src/interface/attachmentInterface.ts
@@ -1,7 +1,6 @@
-import { Document, Schema } from "mongoose";
+import { Document } from "mongoose";
 
 export default interface AttachmentInterface extends Document {
-  // listing: Schema.Types.ObjectId;
   name: string;
   type: "audio" | "video" | "picture" | "document" | "other";
   category: "listing" | "promotion";
