4cc099dded2b5ca5f3e81f5ce8a18978ca0eea75
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-04 22:09:00 +0100

    fix: modify generate idempotency method

--- listingAPI/httpClient.ts
+++ listingAPI/httpClient.ts
@@ -25,8 +25,14 @@ class HttpClient {
     this.httpHeaders = { ...httpHeaders };
   }
 
-  private generateIdempotencyKey(): string {
-    return crypto.randomBytes(16).toString("hex");
+  private generateIdempotencyKey(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) reject(err);
+
+        resolve(buf.toString("hex"));
+      });
+    });
   }
 
   private async call(options: HttpOptions, payload?: any): Promise<any> {
