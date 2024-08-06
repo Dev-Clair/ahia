c43bdfb2d378d2e68b9c9791ecf1a976eb974301
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-06 22:02:26 +0100

    fix: modify script

--- listingAPI/src/utils/cryptoHash.ts
+++ listingAPI/src/utils/cryptoHash.ts
@@ -1,16 +1,14 @@
 import { createHmac } from "node:crypto";
-import Config from "../../config";
 
 /**
- * Creates a cryptographically secure hash of a secret
+ * Creates a cryptographically secure hash of a secret and key
  * @param secret
+ * @param key
  * @returns Promise<string>
  */
-const CryptoHash = (secret: string): Promise<string> => {
+const CryptoHash = (secret: string, key: string): Promise<string> => {
   return new Promise((resolve) => {
-    resolve(
-      createHmac("sha256", Config.APP_SECRET).update(secret).digest("hex")
-    );
+    resolve(createHmac("sha256", key).update(secret).digest("hex"));
   });
 };
 
