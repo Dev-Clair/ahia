ad06a3f0dc45537468389a8bfeb512c66de65430
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-06 20:56:24 +0100

    fix: modify crypto hash function

--- listingAPI/src/utils/cryptoHash.ts
+++ listingAPI/src/utils/cryptoHash.ts
@@ -1,8 +1,16 @@
-import { createHash } from "node:crypto";
+import { createHmac } from "node:crypto";
+import Config from "../../config";
 
+/**
+ * Creates a cryptographically secure hash of a secret
+ * @param secret
+ * @returns Promise<string>
+ */
 const CryptoHash = (secret: string): Promise<string> => {
   return new Promise((resolve) => {
-    resolve(createHash("sha256").update(secret).digest("hex"));
+    resolve(
+      createHmac("sha256", Config.APP_SECRET).update(secret).digest("hex")
+    );
   });
 };
 
