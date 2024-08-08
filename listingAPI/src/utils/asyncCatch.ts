3283739a282ec863f343d06cf92938b380d7307d
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-08 23:04:22 +0100

    fix: modify script to use transactions

--- listingAPI/src/utils/asyncCatch.ts
+++ listingAPI/src/utils/asyncCatch.ts
@@ -1,28 +1,87 @@
+// import { NextFunction, Request, Response } from "express";
+
+// /**
+//  * Handles synchronous and asynchronous errors
+//  * @param Operation
+//  * @param Retry
+//  * @param RetryOptions
+//  * @returns Promise<Response|void>
+//  */
+// const AsyncCatch = (
+//   Operation: (
+//     req: Request,
+//     res: Response,
+//     next: NextFunction
+//   ) => Promise<Response | void>,
+//   Retry?: Function,
+//   RetryOptions?: {}
+// ) => {
+//   return (req: Request, res: Response, next: NextFunction) => {
+//     const executeOperation = () => Operation(req, res, next);
+
+//     if (Retry) {
+//       Retry(executeOperation, RetryOptions).catch(next);
+//     } else {
+//       executeOperation().catch(next);
+//     }
+//   };
+// };
+
+// export default AsyncCatch;
+
 import { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
 
 /**
- * Handles synchronous and asynchronous errors
+ * Handles synchronous and asynchronous errors and manages transactions if needed.
  * @param Operation
  * @param Retry
  * @param RetryOptions
+ * @param UseTransaction
  * @returns Promise<Response|void>
  */
 const AsyncCatch = (
   Operation: (
     req: Request,
     res: Response,
-    next: NextFunction
+    next: NextFunction,
+    session?: mongoose.ClientSession
   ) => Promise<Response | void>,
   Retry?: Function,
-  RetryOptions?: {}
+  RetryOptions?: object,
+  UseTransaction: boolean = false
 ) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const executeOperation = () => Operation(req, res, next);
+  return async (req: Request, res: Response, next: NextFunction) => {
+    let session: mongoose.ClientSession | undefined;
 
-    if (Retry) {
-      Retry(executeOperation, RetryOptions).catch(next);
-    } else {
-      executeOperation().catch(next);
+    if (UseTransaction) {
+      session = await mongoose.startSession();
+
+      session.startTransaction();
+    }
+
+    try {
+      const executeOperation = () => Operation(req, res, next, session);
+
+      if (Retry) {
+        await Retry(executeOperation, RetryOptions);
+      } else {
+        await executeOperation();
+      }
+
+      if (UseTransaction) {
+        await session?.commitTransaction();
+      }
+    } catch (err: any) {
+      if (UseTransaction) {
+        await session?.abortTransaction();
+      }
+
+      next(err);
+    } finally {
+      if (UseTransaction) {
+        session?.endSession();
+      }
     }
   };
 };
