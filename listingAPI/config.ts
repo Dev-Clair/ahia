310ab27dccbe17542cf71ce21c7e6b6bd35a2cb7
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-09 22:40:49 +0100

    fix: modify config script

--- listingAPI/config.ts
+++ listingAPI/config.ts
@@ -8,27 +8,11 @@ if (process.env.NODE_ENV !== "production") {
 const Config = {
   APP_SECRET: process.env.APP_SECRET || "",
   AWS: {
-    REGION: process.env.AWS_REGION || "af-south-1",
     ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID || "",
     SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || "",
     S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME || "",
     S3_FOLDER_NAME: process.env.AWS_S3_FOLDER_NAME || "",
-  },
-  PORT: {
-    HTTP: process.env.HTTP_PORT || 4999,
-    HTTPS: process.env.HTTPS_PORT || 5000,
-  },
-  MONGO_URI: process.env.MONGO_URI || "",
-  NODE_ENV: process.env.NODE_ENV || "test",
-  PAYMENT_SERVICE_URL:
-    process.env.PAYMENT_SERVICE_URL || "127.0.0.1:6999/api/v1/payments",
-  SERVICE: {
-    NAME: process.env.SERVICE_NAME || "",
-    SECRET: process.env.SERVICE_SECRET || "",
-  },
-  SSL: {
-    KEY_FILE_PATH: process.env.SSL_KEY_FILE_PATH || "",
-    CERT_FILE_PATH: process.env.SSL_CERT_FILE_PATH || "",
+    REGION: process.env.AWS_REGION || "af-south-1",
   },
   LISTING: {
     ADMIN_EMAIL_I: process.env.LISTING_ADMIN_EMAIL_I || "",
@@ -37,6 +21,18 @@ const Config = {
       NAME: process.env.SERVICE_NAME || "",
       SECRET: process.env.SERVICE_SECRET || "",
     },
+  },
+  MONGO_URI: process.env.MONGO_URI || "",
+  NODE_ENV: process.env.NODE_ENV || "test",
+  PAYMENT_SERVICE_URL:
+    process.env.PAYMENT_SERVICE_URL || "127.0.0.1:6999/api/v1/payments",
+  PORT: {
+    HTTP: process.env.HTTP_PORT || 4999,
+    HTTPS: process.env.HTTPS_PORT || 5000,
+  },
+  SSL: {
+    KEY_FILE_PATH: process.env.SSL_KEY_FILE_PATH || "",
+    CERT_FILE_PATH: process.env.SSL_CERT_FILE_PATH || "",
   },
 };
 
--- listingExpiryCron/config.js
+++ listingExpiryCron/config.js
@@ -5,16 +5,20 @@ dotenv.config();
 
 const Config = {
   AWS: {
-    REGION: process.env.AWS_REGION,
     ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    REGION: process.env.AWS_REGION,
   },
-  LISTING_NOTIFICATION_EMAIL: process.env.TOUR_NOTIFICATION_EMAIL || "",
-  LISTING_ADMIN_EMAIL_I: process.env.TOUR_ADMIN_EMAIL_I || "",
+  LISTING: {
+    ADMIN_EMAIL_I: process.env.TOUR_ADMIN_EMAIL_I || "",
+    NOTIFICATION_EMAIL: process.env.TOUR_NOTIFICATION_EMAIL || "",
+    SERVICE: {
+      NAME: process.env.SERVICE_NAME,
+      SECRET: process.env.SERVICE_SECRET,
+    },
+  },
   MONGO_URI: process.env.MONGO_URI || "",
   NODE_ENV: process.env.NODE_ENV || "",
-  SERVICE_NAME: process.env.SERVICE_NAME,
-  SERVICE_SECRET: process.env.SERVICE_SECRET,
 };
 
 module.exports = Config;
