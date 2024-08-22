4e3cf0355a76faf9b4a94f0caa20fdb39eda6c11
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-22 21:12:48 +0100

    fix: modify app script

--- tourAPI/app.ts
+++ tourAPI/app.ts
@@ -1,13 +1,17 @@
-import express, { Request, Response, Router, NextFunction } from "express";
+import * as Sentry from "@sentry/node";
+import express, { Request, Response, NextFunction } from "express";
 import express_mongo_sanitize from "express-mongo-sanitize";
 import helmet from "helmet";
 import hpp from "hpp";
 import APIError from "./src/error/apiError";
 import GlobalErrorHandler from "./src/middleware/globalErrorHandlingMiddleware.ts";
-import HttpStatusCode from "./src/enum/httpStatusCode";
+import HttpCode from "./src/enum/httpCode";
+import HttpStatus from "./src/enum/httpStatus";
 import TourRouter from "./src/router";
 
 const App = express();
+
+App.use(Sentry.expressErrorHandler());
 
 App.use(express.json());
 
@@ -25,22 +29,22 @@ App.use(
   (err: APIError | Error, req: Request, res: Response, next: NextFunction) => {
     if (GlobalErrorHandler.isSyntaxError(err)) {
       return res
-        .status(HttpStatusCode.BAD_REQUEST)
+        .status(HttpCode.BAD_REQUEST)
         .json({ error: { name: err.name, message: "Bad or Malformed JSON" } });
     }
 
     if (GlobalErrorHandler.isSafeError(err)) {
       if (err.name === "ValidationError") {
-        return res.status(HttpStatusCode.UNPROCESSABLE_ENTITY).json({
+        return res.status(HttpCode.UNPROCESSABLE_ENTITY).json({
           error: { name: err.name, message: err.message },
         });
       } else if (err.name === "CastError") {
-        return res.status(HttpStatusCode.UNPROCESSABLE_ENTITY).json({
+        return res.status(HttpCode.UNPROCESSABLE_ENTITY).json({
           error: { name: err.name, message: "Invalid ID format" },
         });
       } else {
-        return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
-          err: {
+        return res.status(HttpCode.INTERNAL_SERVER_ERROR).json({
+          error: {
             name: err.name,
             message:
               "Oops! Sorry an error occured on our end, we cannot process your request at this time. Please try again shortly",
@@ -51,16 +55,16 @@ App.use(
 
     if (GlobalErrorHandler.isTrustedError(err as APIError)) {
       return res
-        .status((err as APIError).httpStatusCode)
-        .json({ name: { error: err.name, message: err.message } });
+        .status((err as APIError).code)
+        .json({ error: { name: err.name, message: err.message } });
     }
 
     GlobalErrorHandler.handleError(err);
 
     if (!res.headersSent) {
-      return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+      return res.status(HttpCode.INTERNAL_SERVER_ERROR).json({
         error: {
-          name: "INTERNAL SERVER ERROR",
+          name: HttpStatus.INTERNAL_SERVER_ERROR,
           message:
             "Oops! Sorry an error occured on our end, we cannot process your request at this time. Please try again shortly",
         },
@@ -70,8 +74,11 @@ App.use(
 );
 
 App.all("*", (req: Request, res: Response, next: NextFunction) => {
-  return res.status(HttpStatusCode.NOT_FOUND).json({
-    error: `No resource or route defined for ${req.originalUrl}`,
+  return res.status(HttpCode.NOT_FOUND).json({
+    error: {
+      name: HttpStatus.NOT_FOUND,
+      message: `No resource or route defined for ${req.originalUrl}`,
+    },
   });
 });
 
