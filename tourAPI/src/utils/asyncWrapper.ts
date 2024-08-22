e8a1876f0cfc62686a8d1e5250cf9be5c89e1a51
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-22 20:51:36 +0100

    fix: modiy async catch

---
+++ tourAPI/src/utils/asyncWrapper.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+
+/**
+ * Propagates operational and non-operational errors to the custom global error handling middleware.
+ *
+ */
+class AsyncWrapper {
+  /**
+   * Catch errors from async route handlers
+   * @param Operation
+   * @param Retry
+   * @param RetryOptions
+   * @returns Function
+   */
+  static Catch(
+    Operation: (
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) => Promise<Response | void>,
+    Retry?: Function,
+    RetryOptions?: object
+  ) {
+    return (req: Request, res: Response, next: NextFunction) => {
+      const executeOperation = () => Operation(req, res, next);
+
+      if (Retry) {
+        Retry(executeOperation, RetryOptions).catch(next);
+      } else {
+        executeOperation().catch(next);
+      }
+    };
+  }
+}
+
+export default AsyncWrapper;
