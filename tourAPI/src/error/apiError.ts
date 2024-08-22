db38d8f85e31c55ff471ee0969321e2452694ef3
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-22 20:47:41 +0100

    fix: modify enum

--- tourAPI/src/error/apiError.ts
+++ tourAPI/src/error/apiError.ts
@@ -1,14 +1,15 @@
 import BaseError from "./baseError";
-import HttpStatusCode from "../enum/httpStatusCode";
+import HttpCode from "../enum/httpCode";
+import HttpStatus from "../enum/httpStatus";
 
 class APIError extends BaseError {
   constructor(
-    name: string,
-    httpStatusCode: number | HttpStatusCode,
+    name: string | HttpStatus,
+    code: number | HttpCode,
     isOperational: boolean,
     message: string
   ) {
-    super(name, httpStatusCode, isOperational, message);
+    super(name, code, isOperational, message);
   }
 }
 
