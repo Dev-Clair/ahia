c67794e00450fdbd28b6aeaa33ad057541e39dea
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-22 20:50:44 +0100

    fix: encapsulate hashing logic

---
+++ tourAPI/src/utils/secretManager.ts
@@ -0,0 +1,37 @@
+import { createHmac } from "node:crypto";
+
+/**
+ * Creates and verifies cryptographically secure hashes
+ */
+class SecretManager {
+  /**
+   * Creates a cryptographically secure hash of a value with sha256 algorithm and a (user-defined) secret key
+   * @param value
+   * @param key
+   * @returns Promise<string>
+   */
+  static async HashSecret(value: string, key: string): Promise<string> {
+    return new Promise((resolve) => {
+      resolve(createHmac("sha256", key).update(value).digest("hex"));
+    });
+  }
+
+  /**
+   * Verifies an API service secret
+   * @param secret
+   * @param value
+   * @param key
+   * @returns Promise<string>
+   */
+  static async VerifySecret(
+    secret: string,
+    value: string,
+    key: string
+  ): Promise<boolean> {
+    const hashedValue = await this.HashSecret(value, key);
+
+    return secret === hashedValue;
+  }
+}
+
+export default SecretManager;
