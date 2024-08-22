ccbd88eb5c4e6d2b663c63bd41a9bd0aea7d4c12
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-22 20:51:02 +0100

    fix: http util scripts

---
+++ tourAPI/src/utils/httpClient.ts
@@ -0,0 +1,115 @@
+import crypto from "node:crypto";
+import https from "node:https";
+import FailureRetry from "./failureRetry";
+
+interface HttpOptions {
+  hostname: string;
+  path: string;
+  method?: string;
+  headers?: Record<string, string>;
+}
+
+class HttpClient {
+  private httpOptions: HttpOptions;
+
+  private httpHeaders: object;
+
+  constructor(url: string, httpHeaders: object = {}) {
+    const parsedUrl = new URL(url);
+
+    this.httpOptions = {
+      hostname: parsedUrl.hostname,
+      path: parsedUrl.pathname + parsedUrl.search,
+    };
+
+    this.httpHeaders = { ...httpHeaders };
+  }
+
+  private generateIdempotencyKey(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) reject(err);
+
+        resolve(buf.toString("hex"));
+      });
+    });
+  }
+
+  private async call(options: HttpOptions, payload?: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const req = https.request(options, (res) => {
+        let data = "";
+
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        res.on("end", () => {
+          try {
+            const parsedData = JSON.parse(data);
+            resolve({
+              statusCode: res.statusCode,
+              body: parsedData,
+            });
+          } catch (error) {
+            resolve({
+              statusCode: res.statusCode,
+              body: data,
+            });
+          }
+        });
+      });
+
+      req.on("error", (err) => {
+        reject({
+          statusCode: 500,
+          body: "Error: " + err.message,
+        });
+      });
+
+      if (payload) req.write(JSON.stringify(payload));
+
+      req.end();
+    });
+  }
+
+  private async request(method: string, payload?: any): Promise<any> {
+    const headers = this.httpHeaders;
+
+    if (method.toUpperCase() === "POST" || method.toUpperCase() === "PATCH") {
+      Object.assign(headers, {
+        "idempotency-key": await this.generateIdempotencyKey(),
+      });
+    }
+
+    const options = this.httpOptions;
+
+    Object.assign(options, { method: method });
+
+    Object.assign(options, { headers: headers });
+
+    return FailureRetry.ExponentialBackoff(() => this.call(options, payload));
+  }
+
+  public Get(): Promise<any> {
+    return this.request("GET");
+  }
+
+  public Post(payload: any): Promise<any> {
+    return this.request("POST", payload);
+  }
+
+  public Put(payload: any): Promise<any> {
+    return this.request("PUT", payload);
+  }
+
+  public Patch(payload: any): Promise<any> {
+    return this.request("PATCH", payload);
+  }
+
+  public Delete(): Promise<any> {
+    return this.request("DELETE");
+  }
+}
+
+export default HttpClient;
---
+++ tourAPI/src/utils/httpServer.ts
@@ -0,0 +1,51 @@
+import http from "node:http";
+import https from "node:https";
+import { Express } from "express";
+
+class HttpServer {
+  private app: Express;
+
+  private sslOptions: object;
+
+  private httpServer: http.Server | null = null;
+
+  private httpsServer: https.Server | null = null;
+
+  constructor(App: Express, SSLOptions: object) {
+    this.app = App;
+
+    this.sslOptions = SSLOptions;
+  }
+
+  public startHTTP(HTTP_PORT: string | number) {
+    this.httpServer = http.createServer(this.app);
+
+    this.httpServer.listen(HTTP_PORT, () => {
+      console.log(`Listening on http port ${HTTP_PORT}`);
+    });
+  }
+
+  public startHTTPS(HTTPS_PORT: string | number) {
+    this.httpsServer = https.createServer(this.sslOptions, this.app);
+
+    this.httpsServer.listen(HTTPS_PORT, () => {
+      console.log(`Listening on https port ${HTTPS_PORT}`);
+    });
+  }
+
+  public closeAllConnections() {
+    if (this.httpServer) {
+      this.httpServer.close(() => {
+        console.log("HTTP Server Closed");
+      });
+    }
+
+    if (this.httpsServer) {
+      this.httpsServer.close(() => {
+        console.log("HTTPS Server Closed");
+      });
+    }
+  }
+}
+
+export default HttpServer;
