29c44ace0aa2edc4e6196603e01c05b3ac1ecd23
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-22 13:40:25 +0100

    fix: modify error script

--- listingAPI/src/error/badrequestError.ts
+++ listingAPI/src/error/badrequestError.ts
@@ -1,9 +1,10 @@
 import APIError from "./apiError";
 import HttpCode from "../enum/httpCode";
+import HttpStatus from "../enum/httpStatus";
 
 class BadRequestError extends APIError {
-  constructor(httpCode: number | HttpCode, message: string) {
-    super("BAD REQUEST", httpCode, true, message);
+  constructor(message: string) {
+    super(HttpStatus.BAD_REQUEST, HttpCode.BAD_REQUEST, true, message);
   }
 }
 
