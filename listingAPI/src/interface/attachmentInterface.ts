7756716056a37675b9b14d7d565e2a7b18f4df45
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-28 22:03:53 +0100

    fix: remove redundant schema scripts

--- listingAPI/src/interface/attachmentInterface.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from "mongoose";
-
-export default interface AttachmentInterface extends Document {
-  name: string;
-  type: "audio" | "video" | "picture" | "document" | "other";
-  category: "listing" | "promotion";
-  key: string;
-}
