9deba4128f4792d1b78d78b86c1d404abfb7ce8c
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-29 14:29:35 +0100

    fix: refactor to class with facotry method

--- tourCron/src/service/connection.js
+++ tourCron/src/service/connection.js
@@ -3,53 +3,60 @@ const ConnectionError = require("../error/connectionError");
 const Retry = require("../utils/retry");
 
 /**
- * Establishes connection to database
- * @param {*} connectionUri
+ * Connection
+ * @method connect
+ * @method getConnection
+ * @method Create
  */
-const establishConnection = async (connectionUri) => {
-  if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(connectionUri, {
-      serverSelectionTimeoutMS: 10000,
-      maxPoolSize: 120,
-      minPoolSize: 20,
-      socketTimeoutMS: 60000,
-    });
-  }
-};
+class Connection {
+  connectionUri;
 
-/**
- * Provides connection resource with round robin retry strategy on failure
- * @param {*} connectionUri
- */
-const Connection = async (connectionUri) => {
-  try {
-    await Retry.ExponentialBackoff(() => establishConnection(connectionUri));
-  } catch (err) {
-    try {
-      await Retry.LinearJitterBackoff(() => establishConnection(connectionUri));
-    } catch (err) {
-      throw new ConnectionError(
-        err.message,
-        "Retry strategies failed. Could not establish connection to the database"
-      );
+  constructor(connectionUri) {
+    this.connectionUri = connectionUri;
+  }
+
+  /**
+   * Establishes connection to the database
+   * @private
+   */
+  async connect() {
+    if (mongoose.connection.readyState === 0) {
+      await mongoose.connect(this.connectionUri, {
+        serverSelectionTimeoutMS: 10000,
+        maxPoolSize: 120,
+        minPoolSize: 20,
+        socketTimeoutMS: 60000,
+      });
     }
   }
-};
 
-mongoose.connection.on("connecting", () =>
-  console.log(`Attempting connection to database`)
-);
+  /**
+   * Provides connection resource with round-robin retry strategy on failure
+   * @public
+   */
+  async getConnection() {
+    try {
+      await Retry.ExponentialBackoff(() => this.connect());
+    } catch (err) {
+      try {
+        await Retry.LinearJitterBackoff(() => this.connect());
+      } catch (err) {
+        throw new ConnectionError(
+          err.message,
+          "Retry strategies failed. Could not establish connection to the database."
+        );
+      }
+    }
+  }
 
-mongoose.connection.on("connected", () =>
-  console.log(`Database connection successful`)
-);
+  /**
+   * Creates and returns a new instance of the Connection class
+   * @param connectionUri
+   * @returns Connection
+   */
+  static Create(connectionUri) {
+    return new Connection(connectionUri);
+  }
+}
 
-mongoose.connection.on("disconnected", () =>
-  console.error(`Database connection failure`)
-);
-
-mongoose.connection.on("reconnected", () =>
-  console.log(`Database reconnection successful`)
-);
-
-module.exports = Connection;
+export default Connection;
