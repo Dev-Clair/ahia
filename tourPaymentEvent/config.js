b3f13ce0d1e8b361d36de2ca9fb29b95c207a2dd
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-30 13:28:42 +0100

    fix" modify config

--- tourPaymentEvent/config.js
+++ tourPaymentEvent/config.js
@@ -15,7 +15,6 @@ const Config = {
   MONGO_URI: process.env.MONGO_URI || "",
   NODE_ENV: process.env.NODE_ENV || "",
   TOUR: {
-    NOTIFICATION_EMAIL: process.env.TOUR_NOTIFICATION_EMAIL || "",
     PAYMENT_EVENT_SENTRY_DSN: process.env.TOUR_PAYMENT_EVENT_SENTRY_DSN,
     SERVICE: {
       NAME: process.env.TOUR_SERVICE_NAME,
