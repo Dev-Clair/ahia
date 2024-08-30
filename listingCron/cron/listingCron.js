8dfa153289fa33107bf4050d2be32395b4a5550e
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-08-30 13:13:28 +0100

    fix: refactor script

--- listingCron/cron/listingCron.js
+++ listingCron/cron/listingCron.js
@@ -1,37 +1,24 @@
 const mongoose = require("mongoose");
-const Cache = require("../src/service/cache");
 const Listing = require("../src/model/listing");
 const ListingGenerator = require("./listingGenerator");
 const Retry = require("../src/utils/retry");
 
 const ListingCron = async () => {
-  const deleteCache = new Cache.SetCache();
-
   const listingGenerator = ListingGenerator();
 
   for await (const listing of listingGenerator) {
+    const id = listing._id;
+
     const session = await mongoose.startSession();
 
     const removeListing = await session.withTransaction(async () => {
-      const listing = await Listing.findByIdAndDelete(
-        { _id: Listing._id },
-        { session }
-      );
+      const record = await Listing.findByIdAndDelete({ _id: id }, { session });
 
-      if (!listing) throw new Error(`No record found for listing ${id}`);
+      if (!record) throw new Error(`No record found for listing: ${id}`);
     });
 
     await Retry.ExponentialBackoff(() => removeListing);
-
-    deleteCache.set(provider.email, listing.name);
   }
-
-  return {
-    log: {
-      count: deleteCache.size(),
-      result: deleteCache.entries(),
-    },
-  };
 };
 
 module.exports = ListingCron;
