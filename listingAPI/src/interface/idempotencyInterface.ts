aa21d0f9e411996e76c921694eeabf682546d592
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-09-04 11:46:53 +0100

    fix: separate concerns

--- listingAPI/src/interface/idempotencyInterface.ts
+++
@@ -1,6 +0,0 @@
-import { Document } from "mongoose";
-
-export default interface IdempotencyInterface extends Document {
-  key: string;
-  createdAt: Date;
-}
--- listingAPI/src/model/listingModel.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import ListingSchema from "../schema/listingSchema";
-import ListingInterface from "../interface/listingInterface";
-
-const Listing = mongoose.model<ListingInterface>("Listing", ListingSchema);
-
-export default Listing;
