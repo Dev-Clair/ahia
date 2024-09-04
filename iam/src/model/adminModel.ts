64422c5964869a4ce68a503f4b48ca04f833cf84
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-09-04 11:45:44 +0100

    fix: separate concerns

--- iam/src/model/adminModel.ts
+++
@@ -1,7 +0,0 @@
-import IAM from "./iamModel";
-import AdminSchema from "../schema/adminSchema";
-import AdminInterface from "../interface/adminInterface";
-
-const Admin = IAM.discriminator<AdminInterface>("Admin", AdminSchema);
-
-export default Admin;
--- iam/src/model/realtorModel.ts
+++
@@ -1,7 +0,0 @@
-import IAM from "./iamModel";
-import RealtorSchema from "../schema/realtorSchema";
-import RealtorInterface from "../interface/realtorInterface";
-
-const Realtor = IAM.discriminator<RealtorInterface>("Realtor", RealtorSchema);
-
-export default Realtor;
