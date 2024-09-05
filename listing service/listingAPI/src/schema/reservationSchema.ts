dd5030f08cb77cc8c62280d0b8111cc94c480bdb
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-09-05 13:48:51 +0100

    fix: reservation type listing model

--- listing service/listingAPI/src/schema/reservationSchema.ts
+++ listing service/listingAPI/src/schema/reservationSchema.ts
@@ -1,3 +1,18 @@
 import { Schema } from "mongoose";
-import ListingInterface from "../interface/listingInterface";
 import ReservationInterface from "../interface/reservationInterface";
+
+const ReservationSchema: Schema<ReservationInterface> = new Schema({
+  booking: {
+    plan: {
+      type: [String],
+      enum: ["daily", "extended"],
+      required: true,
+    },
+    termsAndConditions: {
+      type: [String],
+      required: false,
+    },
+  },
+});
+
+export default ReservationSchema;
