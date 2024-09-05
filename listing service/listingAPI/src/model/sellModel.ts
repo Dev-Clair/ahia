da2ab2a7231fbb7e35d42aa36667c4efd2604efe
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-09-05 13:47:18 +0100

    fix: sell type listing model

--- listing service/listingAPI/src/model/sellModel.ts
+++ listing service/listingAPI/src/model/sellModel.ts
@@ -1,3 +1,7 @@
-import mongoose from "mongoose";
+import Listing from "./listingModel";
 import SellSchema from "../schema/sellSchema";
 import SellInterface from "../interface/sellInterface";
+
+const Sell = Listing.discriminator<SellInterface>("Sell", SellSchema);
+
+export default Sell;
--- listing service/listingAPI/src/schema/sellSchema.ts
+++ listing service/listingAPI/src/schema/sellSchema.ts
@@ -1,3 +1,22 @@
 import { Schema } from "mongoose";
 import SellInterface from "../interface/sellInterface";
-import ListingInterface from "../interface/listingInterface";
+
+const SellSchema: Schema<SellInterface> = new Schema({
+  isNegotiable: {
+    type: Boolean,
+    required: true,
+  },
+  mortgage: {
+    plan: {
+      type: [String],
+      enum: ["short", "medium", "long"],
+      required: true,
+    },
+    termsAndConditions: {
+      type: [String],
+      required: false,
+    },
+  },
+});
+
+export default SellSchema;
