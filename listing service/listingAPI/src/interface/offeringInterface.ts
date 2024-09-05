7803d53fbed5e497bb707eaad1f616ca30ceac7a
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-09-05 13:49:52 +0100

    feat: create offering sub document

--- listing service/listingAPI/src/interface/offeringInterface.ts
+++ listing service/listingAPI/src/interface/offeringInterface.ts
@@ -1,8 +1,12 @@
-import { Document } from "mongoose";
+import { Document, Schema } from "mongoose";
 
 export default interface OfferingInterface extends Document {
-  id: string;
   type: string;
+  price: number;
   features: string[];
   status: "open" | "closed";
+  media: {
+    image: string; // Review: Set min number of images that can exist for an offering
+    video: string; // Review: Set min number of videos that can exist for an offering
+  };
 }
