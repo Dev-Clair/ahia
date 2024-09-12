ee8180a60ac04b435a8d7f881c76b1ddd3d62734
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-09-12 21:30:19 +0100

    fix: custom type extension

--- listing service/listingAPI/src/type/express.d.ts
+++ listing service/listingAPI/src/type/express.d.ts
@@ -2,9 +2,11 @@ import { Request } from "express";
 import ListingInterface from "../interface/listingInterface";
 import ListingService from "../service/listingService";
 
-declare module "express-serve-static-core" {
-  interface Request {
-    listing?: ListingInterface;
-    service?: ListingService;
+declare global {
+  namespace Express {
+    interface Request {
+      listing?: ListingInterface;
+      service?: ListingService;
+    }
   }
 }
