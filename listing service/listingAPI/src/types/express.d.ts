a6014aeb243e93a09f0a4ae0bdd0c9ecfc04be6a
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-10-04 11:14:48 +0100

    fix: add offering to request object

--- listing service/listingAPI/src/types/express.d.ts
+++ listing service/listingAPI/src/types/express.d.ts
@@ -1,13 +1,14 @@
 import { Request } from "express";
 import IListing from "../interface/IListing";
-import ListingService from "../service/listingService";
+import IOffering from "../interface/IOffering";
 
 export {};
 
 declare global {
   namespace Express {
     export interface Request {
-      listing: ListingInterface;
+      listing?: IListing;
+      offering?: IOffering;
     }
   }
 }
