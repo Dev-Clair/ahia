47efb545a79c9e22dccd0712c57720677878bd6b
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-10-05 18:34:13 +0100

    fix: add promotions to document resolver

--- listing service/listingAPI/src/types/express.d.ts
+++ listing service/listingAPI/src/types/express.d.ts
@@ -1,6 +1,7 @@
 import { Request } from "express";
 import IListing from "../interface/IListing";
 import IOffering from "../interface/IOffering";
+import IPromotion from "../interface/IPromotion";
 
 export {};
 
@@ -9,6 +10,7 @@ declare global {
     export interface Request {
       listing?: IListing;
       offering?: IOffering;
+      promotion?: IPromotion;
     }
   }
 }
