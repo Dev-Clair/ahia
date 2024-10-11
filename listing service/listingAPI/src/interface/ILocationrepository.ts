c2df9c7797f08e63e3c84d231fb7e05c6f60d89d
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-10-11 12:00:17 +0100

    feat: create persitent storage for location service

---
+++ listing service/listingAPI/src/interface/ILocationrepository.ts
@@ -0,0 +1,53 @@
+import ILocation from "./ILocation";
+import IRepository from "./IRepository";
+
+export default interface ILocationRepository extends IRepository<ILocation> {
+  /**
+   * Retrieves a collection of location documents
+   * @param queryString query object
+   * @param options configuration options
+   */
+  findAll(
+    queryString: Record<string, any>,
+    options?: { [key: string]: any }
+  ): Promise<ILocation[]>;
+
+  /**
+   * Retrieves a location by id
+   * @param id location id
+   * @param options configuration options
+   */
+  findById(
+    id: string,
+    options?: { [key: string]: any }
+  ): Promise<ILocation | null>;
+
+  /**
+   * Creates a new location in collection
+   * @param payload data object
+   * @param options configuration options
+   */
+  save(
+    payload: Partial<ILocation>,
+    options?: { [key: string]: any }
+  ): Promise<string>;
+
+  /**
+   * Updates a location by id
+   * @param id location id
+   * @param payload data object
+   * @param options configuration options
+   */
+  update(
+    id: string,
+    payload: Partial<ILocation | any>,
+    options?: { [key: string]: any }
+  ): Promise<string>;
+
+  /**
+   * Deletes a location by id
+   * @param id location id
+   * @param options configuration options
+   */
+  delete(id: string, options?: { [key: string]: any }): Promise<string>;
+}
---
+++ listing service/listingAPI/src/schema/locationSchema.ts
@@ -0,0 +1,34 @@
+import { Schema } from "mongoose";
+import ILocation from "../interface/ILocation";
+
+const LocationSchema: Schema<ILocation> = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    coordinates: {
+      lat: {
+        type: Number,
+        required: true,
+        min: -90,
+        max: 90,
+      },
+      lng: {
+        type: Number,
+        required: true,
+        min: -180,
+        max: 180,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+LocationSchema.index({ coordinates: "2dsphere" });
+
+export default LocationSchema;
