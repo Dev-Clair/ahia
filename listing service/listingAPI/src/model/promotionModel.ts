6faed4569774ab3c1d08306a25a1d413ef3aad54
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-10-13 20:47:18 +0100

    fix: remove promotion module

--- listing service/listingAPI/src/model/promotionModel.ts
+++
@@ -1,7 +0,0 @@
-// import { model } from "mongoose";
-// import IPromotion from "../interface/IPromotion";
-// import PromotionSchema from "../schema/promotionSchema";
-
-// const Promotion = model<IPromotion>("Promotion", PromotionSchema);
-
-// export default Promotion;
