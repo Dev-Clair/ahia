3f1c3657d0368809c9d10c858e6c784c3f08b538
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-10-18 17:08:05 +0100

    fix: modify import

--- listing service/listingAPI/src/route/listingRoute.ts
+++ listing service/listingAPI/src/route/listingRoute.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import AuthMiddleware from "../middleware/authMiddleware";
 import DocumentMiddleware from "../middleware/documentMiddleware";
 import IdempotencyMiddleware from "../middleware/idempotencyMiddleware";
-import AppMiddleware from "../middleware/appMiddleWare";
+import AppMiddleware from "../middleware/appMiddleware";
 import PaymentverificationMiddleware from "../middleware/paymentverificationMiddleware";
 import ValidationMiddleware from "../middleware/validationMiddleware";
 import ListingController from "../controller/listingController";
