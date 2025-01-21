28e8e6fb288e59dc5eee9d058099e9c71bb3e6e1
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2025-01-21 11:22:36 +0100

    fix: refactor cast error logic

--- listing service/listingAPI/app.ts
+++ listing service/listingAPI/app.ts
@@ -36,11 +36,11 @@ App.use(
     if (GlobalErrorHandler.isSafeError(err)) {
       if (err.name === "ValidationError") {
         return res.status(HttpCode.UNPROCESSABLE_ENTITY).json({
-          error: { name: err.name, message: JSON.parse(err.message) },
+          error: { name: err.name, message: err.message },
         });
       } else if (err.name === "CastError") {
         return res.status(HttpCode.UNPROCESSABLE_ENTITY).json({
-          error: { name: err.name, message: JSON.parse(err.message) },
+          error: { name: err.name, message: "Invalid ID" },
         });
       } else {
         return res.status(HttpCode.INTERNAL_SERVER_ERROR).json({
