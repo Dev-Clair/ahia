a8fed74e5ebe39fc32789f56df913c9f90c9bb61
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2025-01-27 22:01:11 +0100

    fix: modify type def file

--- listing service/listingAPI/src/types/request.d.ts
+++ listing service/listingAPI/src/types/request.d.ts
@@ -1,5 +1,6 @@
 import { Request } from "express";
 import IGeoCoordinates from "../interface/IGeocoordinates";
+import IPaginate from "../interface/IPaginate";
 import IListing from "../interface/IListing";
 import IProduct from "../interface/IProduct";
 
@@ -7,6 +8,7 @@ declare global {
   namespace Express {
     export interface Request {
       idempotent?: Record<string, any>;
+      paginate?: IPaginate;
       geoCoordinates?: IGeoCoordinates;
       listing?: IListing;
       product?: IProduct;
