a8c90f75b5e7b411c297b4c262b1eafdfb7c5c69
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2025-02-07 10:44:37 +0100

    fix: refactor cron scheduler

--- listing service/listingAPI/cron/scheduler.ts
+++ listing service/listingAPI/cron/scheduler.ts
@@ -5,11 +5,11 @@ import { ProductsCollectionCleanUp } from "./jobs/productsCollectionCleanUp";
 export const ListingsJob = cron.schedule(
   "0 23 28 * *",
   ListingsCollectionCleanUp,
-  { runOnInit: true }
+  { runOnInit: false }
 );
 
 export const ProductsJob = cron.schedule(
   "0 */3 * * *",
   ProductsCollectionCleanUp,
-  { runOnInit: true }
+  { runOnInit: false }
 );
