8074fc7cea5da6ba60183138dd511e983dfc185e
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2025-02-09 17:28:24 +0100

    fix: refactor offering interface

--- listing service/listingAPI/src/interface/IOffering.ts
+++ listing service/listingAPI/src/interface/IOffering.ts
@@ -1,7 +1,6 @@
-import IDocument from "./IDocument";
 import Offerings from "../constant/offerings";
 
-export default interface IOffering extends IDocument {
+export default interface IOffering {
   name: keyof typeof Offerings;
   category: "economy" | "premium" | "luxury" | "exclusive";
   features: string[];
