b52295ca68fadc3dedcfe6f53b4122638d2e853a
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2025-03-02 16:58:01 +0100

    fix: refactor express response object extension

--- listing service/listingAPI/src/types/response.d.ts
+++ listing service/listingAPI/src/types/response.d.ts
@@ -3,8 +3,9 @@ import { Response } from "express";
 declare module "express-serve-static-core" {
     export interface Response {
         meta?: {
-            timestamp: string;
-            requestId?: string;
+            request: {
+                id: string; timestamp: string; idempotent?: boolean
+            };
             pagination?: {
                 total: number;
                 limit: number;
