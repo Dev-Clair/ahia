19f19e8989d083a117e6601aa71154813b0e0b74
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2025-03-02 16:58:29 +0100

    fix: refactor to response parser

--- listing service/listingAPI/src/utils/responseParser.ts
+++ listing service/listingAPI/src/utils/responseParser.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import IDocument from "../interface/IDocument";
 
-const Paginator = (req: Request, res: Response, resource: IDocument[]) =>
+const ResponseParser = (req: Request, res: Response, resource: IDocument[]) =>
     res.meta!.pagination = {
         total: resource.length,
 
@@ -12,4 +12,4 @@ const Paginator = (req: Request, res: Response, resource: IDocument[]) =>
         pages: Math.ceil(resource.length / (req.queryString?.limit ? parseInt(req.queryString.limit.toString(), 10) : 10))
     }
 
-export default Paginator;
+export default ResponseParser;
