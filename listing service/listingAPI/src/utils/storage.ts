446f9f33921f32c9aa41d904fbfd366c2abd2bad
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2025-03-06 16:30:17 +0100

    fix: refactor storage util

--- listing service/listingAPI/src/utils/storage.ts
+++ listing service/listingAPI/src/utils/storage.ts
@@ -6,8 +6,6 @@ import {
   S3Client,
   S3ClientConfig,
 } from "@aws-sdk/client-s3";
-import { randomUUID } from "node:crypto";
-import Config from "../../config";
 
 class Storage {
   private s3: S3Client;
@@ -22,18 +20,16 @@ class Storage {
 
   /**
    * Uploads an object to the S3
-   * @param id unique identifier for the object
-   * @param body file buffer or stream
-   * @param contentType  optional content type for the file object
+   * @param key Object identifier
+   * @param body File buffer
+   * @param contentType Object content type (optional)
    */
   public async Upload(
-    id: string,
+    key: string,
     body: Buffer,
     contentType?: string
   ): Promise<string | void> {
     try {
-      const key = `${id}/${randomUUID()}`;
-
       const input = {
         Bucket: this.bucket,
         Key: key,
@@ -60,9 +56,9 @@ class Storage {
 
   /**
    * Downloads an object from S3
-   * @param key key of the object in S3
-   * @param start start byte for range retrieval (optional)
-   * @param end end byte for range retrieval (optional)
+   * @param key Object identifier
+   * @param start Start byte for range retrieval (optional)
+   * @param end End byte for range retrieval (optional)
    */
   public async Download(key: string, start?: string, end?: string) {
     try {
@@ -91,7 +87,7 @@ class Storage {
 
   /**
    * Deletes an object from S3
-   * @param key key of the object to delete
+   * @param key Object Identifier
    */
   public async Delete(key: string): Promise<boolean | undefined> {
     try {
@@ -118,7 +114,7 @@ class Storage {
 
   /**
    * Retrieves a paginated collection of objects from S3
-   * @param prefix prefix to filter objects (optional)
+   * @param prefix Prefix to filter objects (optional)
    */
   public async *RetrieveCollection(
     prefix: string
@@ -146,16 +142,8 @@ class Storage {
   /**
    * Creates and returns a new instance of the Storage class
    */
-  public static Create(): Storage {
-    const configuration: S3ClientConfig = {
-      region: Config.AWS.REGION,
-      credentials: {
-        accessKeyId: Config.AWS.IAM.ACCESS_KEY_ID,
-        secretAccessKey: Config.AWS.IAM.SECRET_ACCESS_KEY,
-      },
-    };
-
-    return new Storage(configuration, Config.AWS.S3_BUCKET_NAME);
+  public static Create(configuration: S3ClientConfig, bucket: string): Storage {
+    return new Storage(configuration, bucket);
   }
 }
 
